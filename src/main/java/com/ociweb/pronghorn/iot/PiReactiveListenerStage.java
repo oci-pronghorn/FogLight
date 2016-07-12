729bc94096fa2a7d9535359207cea3b3ded36d00
alexherriott <alexherriott@wustl.edu>
2016-07-12 12:51:12 -0500

    i2c digital read working

--- src/main/java/com/ociweb/pronghorn/iot/PiReactiveListenerStage.java
+++ src/main/java/com/ociweb/pronghorn/iot/PiReactiveListenerStage.java
@@ -12,44 +12,53 @@ import com.ociweb.pronghorn.stage.scheduling.GraphManager;
 
 public class PiReactiveListenerStage extends ReactiveListenerStage{
 
-
+	private int lastDigital = -1;
+	private int lastAnalog = -1;
+	
+	
 	public PiReactiveListenerStage(GraphManager graphManager, Object listener, Pipe<?>[] inputPipes, Pipe<?>[] outputPipes) {
 		super(graphManager, listener, inputPipes, outputPipes);             
 	}
 
 	@Override
 	protected void consumeI2CMessage(Object listener, Pipe<I2CResponseSchema> p) {
-
 		while (PipeReader.tryReadFragment(p)) {                
-
 			int msgIdx = PipeReader.getMsgIdx(p);
 			switch (msgIdx) {   
 			case I2CResponseSchema.MSG_RESPONSE_10:
 				if (listener instanceof I2CListener || listener instanceof DigitalListener || listener instanceof AnalogListener) {
 					int addr = PipeReader.readInt(p, I2CResponseSchema.MSG_RESPONSE_10_FIELD_ADDRESS_11);
 					int register = PipeReader.readInt(p, I2CResponseSchema.MSG_RESPONSE_10_FIELD_REGISTER_14);
-					int time = PipeReader.readInt(p, I2CResponseSchema.MSG_RESPONSE_10_FIELD_TIME_13);
+					long time = PipeReader.readLong(p, I2CResponseSchema.MSG_RESPONSE_10_FIELD_TIME_13);
 
 					byte[] backing = PipeReader.readBytesBackingArray(p, I2CResponseSchema.MSG_RESPONSE_10_FIELD_BYTEARRAY_12);
 					int position = PipeReader.readBytesPosition(p, I2CResponseSchema.MSG_RESPONSE_10_FIELD_BYTEARRAY_12);
 					int length = PipeReader.readBytesLength(p, I2CResponseSchema.MSG_RESPONSE_10_FIELD_BYTEARRAY_12);
 					int mask = PipeReader.readBytesMask(p, I2CResponseSchema.MSG_RESPONSE_10_FIELD_BYTEARRAY_12);
 
+					
 					if (listener instanceof I2CListener){
 						((I2CListener)listener).i2cEvent(addr, register, time, backing, position, length, mask);
 					}
 					else if(listener instanceof DigitalListener){
 						assert(addr == 4);
 						assert(length == 1);
-
-						System.out.println("received grove digital signal " + backing[position&mask]);
-						((DigitalListener)listener).digitalEvent(register, time, backing[position&mask]);
+						
+						int tempValue = backing[position&mask];
+						if(tempValue!=lastDigital){
+							lastDigital = tempValue;
+							((DigitalListener)listener).digitalEvent(register, time, tempValue);
+						}
 					}else{
 						assert(addr == 4);
 						assert(length == 3);
 
 						byte[] temp = Arrays.copyOfRange(backing, position&mask, (position+3)&mask); //TODO: Does this produce garbage?
-						((AnalogListener)listener).analogEvent(register, time, 0, temp[1]*256+((int)temp[2]&0xFF)); //TODO: Average=?
+						int tempValue = temp[1]*256+((int)temp[2]&0xFF);
+						if(tempValue!=lastAnalog){
+							lastAnalog = tempValue;
+							((AnalogListener)listener).analogEvent(register, time, 0, tempValue); //TODO: Average=?
+						}
 					}
 				}
 				break;

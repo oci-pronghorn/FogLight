acc71b4054a2b6cc2395c4e34279f20782410c24
Nathan Tippy <nathan@tippy.name>
2016-07-17 23:35:20 -0500

    switch to new testing runtime

--- src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -1,38 +1,33 @@
 package ${package};
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.ociweb.iot.hardware.TestHardware;
+import com.ociweb.iot.maker.IOTDeviceRuntime;
+import com.ociweb.pronghorn.stage.scheduling.NonThreadScheduler;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest { 
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	
+	 @Test
+	    public void testApp()
+	    {
+	    	IOTDeviceRuntime runtime = IOTDeviceRuntime.test(new IoTApp());
+	    	    	
+	    	NonThreadScheduler scheduler = (NonThreadScheduler)runtime.getScheduler();    	
+	    
+	    	scheduler.setSingleStepMode(true);
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	    	TestHardware hardware = (TestHardware)runtime.getHardware();
+	    
+	    	
+	    	//test application here
+	    	
+	    }
 }

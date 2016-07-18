42a33dae191182c492e0afa060ad1cca79edfa22
Nathan Tippy <nathan@tippy.name>
2016-07-18 12:31:00 -0500

    remove dead import

--- blinkyUsingLambdas/src/test/java/com/ociweb/iot/project/lightblink/AppTest.java
+++ blinkyUsingLambdas/src/test/java/com/ociweb/iot/project/lightblink/AppTest.java
@@ -3,7 +3,6 @@ package com.ociweb.iot.project.lightblink;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.ociweb.iot.hardware.TestHardware;

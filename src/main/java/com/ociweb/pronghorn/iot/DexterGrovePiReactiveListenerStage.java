2f944a260f7c2b47cf9cb8647f7473fd83573659
alexherriott <alexherriott@wustl.edu>
2016-07-19 09:37:48 -0500

    merging

--- src/main/java/com/ociweb/pronghorn/iot/DexterGrovePiReactiveListenerStage.java
+++ src/main/java/com/ociweb/pronghorn/iot/DexterGrovePiReactiveListenerStage.java
@@ -78,7 +78,8 @@ public class DexterGrovePiReactiveListenerStage extends ReactiveListenerStage{
     						}
                         }
 					}else if(listener instanceof RotaryListener && addr==4 && length==2){
-						int tempValue = (((int)backing[(position+0)&mask])<<8) | (0xFF&((int)backing[(position+1)&mask]));
+						byte[] tempArray = {backing[(position+0)&mask], backing[(position+1)&mask]};
+						int tempValue = (((int)tempArray[0])<<8) | (0xFF&((int)tempArray[1]));
 						((RotaryListener)listener).rotaryEvent(register, time, tempValue, 0, 0);
 					}
 				}

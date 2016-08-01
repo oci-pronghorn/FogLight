56f2aa6e3c6817611c7238a39a724310ad41b809
alexherriott <alexherriott@wustl.edu>
2016-08-01 17:04:06 -0500

    created HardwareConnection base class for i2c and hardconnection

--- src/main/java/com/ociweb/iot/hardware/HardConnection.java
+++ src/main/java/com/ociweb/iot/hardware/HardConnection.java
@@ -1,39 +1,30 @@
 package com.ociweb.iot.hardware;
 
-public class HardConnection {
+public class HardConnection extends HardwareConnection {
 
-    public final IODevice twig;
     public final byte connection;
-    
-    public final int responseMS;
-    public final int movingAverageWindowMS;
     
     
     public static final int DEFAULT_AVERAGE = 1000;
     
     public HardConnection(IODevice twig, int connection) {
-        this.twig = twig;
+    	super(twig, twig.response(), HardConnection.DEFAULT_AVERAGE);
         assert(connection<255 && connection>=0);
         this.connection = (byte)connection;
-        this.responseMS = twig.response();
-        this.movingAverageWindowMS = DEFAULT_AVERAGE;
+
     }
     
     public HardConnection(IODevice twig, int connection, int customResponse) {
-        this.twig = twig;
+    	super(twig, Math.max(customResponse, twig.response()), HardConnection.DEFAULT_AVERAGE);
         assert(connection<255 && connection>=0);
         this.connection = (byte)connection;
-        this.responseMS = Math.max(customResponse, twig.response());
-        this.movingAverageWindowMS = DEFAULT_AVERAGE;
         
     }
     
     public HardConnection(IODevice twig, int connection, int customResponse, int averageWindowMS) {
-        this.twig = twig;
+    	super(twig, Math.max(customResponse, twig.response()), averageWindowMS);
         assert(connection<255 && connection>=0);
         this.connection = (byte)connection;
-        this.responseMS = Math.max(customResponse, twig.response());
-        this.movingAverageWindowMS = averageWindowMS;
     }
     
 }
--- src/main/java/com/ociweb/iot/hardware/I2CConnection.java
+++ src/main/java/com/ociweb/iot/hardware/I2CConnection.java
@@ -1,48 +1,41 @@
 package com.ociweb.iot.hardware;
 
-public class I2CConnection {
+public class I2CConnection extends HardwareConnection {
 
-    public final IODevice twig;
     public final byte address;  		//i2c address
     public final byte[] readCmd;  		//bytes sent to device to prompt a read
     public final int readBytes;  		//number of bytes to be read on a read
     public final int register;			//identifier for register you're reading from. Does not have to match device spec.
     public final byte[] setup;			//setup bytes sent to initialize communications
     
-    public final int responseMS;
-    public final int movingAverageWindowMS;
+    
     
     public I2CConnection(IODevice twig, byte address, byte[] readCmd, int readBytes, int register, byte[] setup) {
-        this.twig = twig;
+        super(twig, twig.response(), HardConnection.DEFAULT_AVERAGE);
         this.address = address;
         this.readCmd = readCmd;
         this.readBytes = readBytes;
         this.register = register;
         this.setup = setup;
-        this.responseMS = twig.response();
-        this.movingAverageWindowMS = HardConnection.DEFAULT_AVERAGE;
     }
     
     public I2CConnection(IODevice twig, byte address, byte[] readCmd, int readBytes, int register, byte[] setup, int responseMS) {
-        this.twig = twig;
+    	super(twig, responseMS, HardConnection.DEFAULT_AVERAGE);
         this.address = address;
         this.readCmd = readCmd;
         this.readBytes = readBytes;
         this.register = register;
         this.setup = setup;
-        this.responseMS = responseMS;
-        this.movingAverageWindowMS = HardConnection.DEFAULT_AVERAGE;
     }
     
     public I2CConnection(IODevice twig, byte address, byte[] readCmd, int readBytes, int register, byte[] setup, int responseMS, int movingAverageWindowMS) {
-        this.twig = twig;
-        this.address = address;
+    	super(twig, responseMS, movingAverageWindowMS);
+    	this.address = address;
         this.readCmd = readCmd;
         this.readBytes = readBytes;
         this.register = register;
         this.setup = setup;
-        this.responseMS = responseMS; 
-        this.movingAverageWindowMS = movingAverageWindowMS;
+        
     }
     
     @Override

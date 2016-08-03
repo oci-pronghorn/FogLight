f3bf2519b8947e35a79ff42074badf7ce51f682c
Nathan Tippy <nathan@tippy.name>
2016-08-03 15:53:46 -0500

    first stopwatch

---
+++ 003-Stopwatch/src/main/java/com/ociweb/iot/stopwatch/IoTApp.java
@@ -0,0 +1,98 @@
+package com.ociweb.iot.stopwatch;
+
+
+import static com.ociweb.iot.grove.GroveTwig.*;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+import com.ociweb.iot.grove.Grove_LCD_RGB;
+import com.ociweb.iot.hardware.Hardware;
+import com.ociweb.iot.maker.CommandChannel;
+import com.ociweb.iot.maker.DeviceRuntime;
+import com.ociweb.iot.maker.IoTSetup;
+
+public class IoTApp implements IoTSetup
+{
+	private static final int BUTTON_CONNECTION = 4; //long press clear, short press start/top
+	
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+	
+	private boolean running;
+	private boolean startOnUp;
+	private long startTime;
+	private long stopTime;
+	
+	private final ZoneId zone = ZoneOffset.UTC;
+	
+    public static void main( String[] args ) {
+        DeviceRuntime.run(new IoTApp());
+    }
+        
+    @Override
+    public void declareConnections(Hardware c) {
+    	c.useConnectD(Button, BUTTON_CONNECTION);
+    	c.useI2C();
+    	c.useTriggerRate(50);
+    }
+
+
+    @Override
+    public void declareBehavior(DeviceRuntime runtime) {
+    	
+    	CommandChannel channel = runtime.newCommandChannel();
+    	
+    	runtime.addDigitalListener((connection, time, durationMillis, value)->{
+    		    		
+    		if (0 == value) {    			
+    			//how long was it pressed before this change to up?
+    			if (durationMillis>1000) {    				
+    				startTime = 0;
+    				stopTime = 0;
+    				running = false;
+    			} else {
+    				running = startOnUp;
+    				startOnUp = false;
+    			}
+    		} else {
+    			//user button down
+    			//toggle clock start or stop
+    			if (!running) {
+    				if (0==startTime) {
+    					startTime = System.currentTimeMillis();
+    				} else {
+    					startTime = System.currentTimeMillis()-(stopTime-startTime);
+    					stopTime = 0;
+    				}
+    				startOnUp = true;
+    			} else {
+    				stopTime = System.currentTimeMillis();
+    				running = false;
+    			}
+    			
+    		}
+    		
+    	});
+    	
+    	final CommandChannel lcdTextChannel = runtime.newCommandChannel();
+    	runtime.addTimeListener((time)->{ 
+    		
+    		    
+    		
+    		  long duration = 0==startTime? 0 :  stopTime==0 ? time-startTime :  stopTime-startTime;
+    		
+    		  LocalDateTime date = LocalDateTime.ofInstant(Instant.ofEpochMilli(duration), zone);
+    		
+    		  String text = date.format(formatter);
+
+			  Grove_LCD_RGB.commandForText(lcdTextChannel, text);
+    		
+    	});
+    	
+    }
+        
+  
+}

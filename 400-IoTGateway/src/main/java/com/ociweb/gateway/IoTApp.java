d7ca79e52944a55bf53f194895a3f2514126c0ca
Nathan Tippy <nathan@tippy.name>
2016-10-01 11:47:51 -0500

    show how to pull in optional command line arguments

--- 400-IoTGateway/src/main/java/com/ociweb/gateway/IoTApp.java
+++ 400-IoTGateway/src/main/java/com/ociweb/gateway/IoTApp.java
@@ -7,8 +7,15 @@ import com.ociweb.iot.maker.IoTSetup;
 
 public class IoTApp implements IoTSetup
 {
+	private static String brokerURI;
+	private static String clientId;
   
     public static void main( String[] args ) {
+    	
+    	//parse the optional command line arguments
+    	brokerURI = DeviceRuntime.getOptArg("--brokerURI", "-br", args, "tcp://localhost:1883");
+    	clientId = DeviceRuntime.getOptArg("--clientId", "-id", args, "TestClient");
+    			
         DeviceRuntime.run(new IoTApp());
     }
     
@@ -21,7 +28,7 @@ public class IoTApp implements IoTSetup
     @Override
     public void declareBehavior(DeviceRuntime runtime) {
 
-    	runtime.addStartupListener(new SubscribeDataMQTT(runtime, "#", "localPub", "tcp://localhost:1883", "TestClient"));;
+    	runtime.addStartupListener(new SubscribeDataMQTT(runtime, "#", "localPub", brokerURI, clientId ));;
     	
     	runtime.addPubSubListener(new PublishKafka(runtime,"kafkaTopic")).addSubscription("localPub");
     

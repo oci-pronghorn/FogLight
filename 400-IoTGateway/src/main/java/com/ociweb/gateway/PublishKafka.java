c9154e44bcf2bb79b2c3b71b6fc2e1ecbef24468
Nathan Tippy <nathan@tippy.name>
2016-10-02 18:23:11 -0500

    updates for build and subscription issues.

--- 400-IoTGateway/src/main/java/com/ociweb/gateway/PublishKafka.java
+++ 400-IoTGateway/src/main/java/com/ociweb/gateway/PublishKafka.java
@@ -1,16 +1,21 @@
 package com.ociweb.gateway;
 
+import java.util.Date;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.ociweb.iot.maker.DeviceRuntime;
 import com.ociweb.iot.maker.PayloadReader;
 import com.ociweb.iot.maker.PubSubListener;
+import com.ociweb.pronghorn.util.Appendables;
 
 public class PublishKafka implements PubSubListener {
 
+	private static final Logger logger = LoggerFactory.getLogger(PublishKafka.class);
 	private final Properties properties=new Properties();
     private final String publishTopic;
 	
@@ -27,22 +32,51 @@ public class PublishKafka implements PubSubListener {
 
 	@Override
 	public void message(CharSequence topic, PayloadReader payload) {
+				
+		String sensorTopic = payload.readUTF();
+
+		CharSequence[] topicNibbles = Appendables.split(sensorTopic, '/');
 		
+		//[0] will be the root
+		//[1] will be the name of the station
+		//[2] will be the type of message either pump or tank
+		
+		StringBuilder builder = new StringBuilder();
+		
+		if ("total".equals(topicNibbles[2])) { //purchase total
+			long time = payload.readLong();
+			String fuelName = payload.readUTF();
+			int priceInCents = payload.readInt();
+			int centiUnits = payload.readInt(); 
+			
+			builder.append(new Date(time)+" "+fuelName+" price "+priceInCents+" units "+centiUnits);
+			
+		}
+		
+		if ("tank".equals(topicNibbles[2])) {
+			long time = payload.readLong();
+			int volumCM2 = payload.readInt();
+			String fuelName = payload.readUTF();
+			
+			builder.append(new Date(time)+" volume "+volumCM2+" "+fuelName);
+			
+		}
+		
+		
+		System.out.println("got message "+sensorTopic+"  payload  "+builder.toString());
+		
+
 		KafkaProducer producer=null;
 		try {
 			producer = new KafkaProducer(properties);
-			
-			String originalTopic = payload.readUTF();
-			byte[] buffer = new byte[payload.available()];
-			payload.read(buffer);
-			
-			producer.send(new ProducerRecord(publishTopic, buffer));
-		
-	   } finally {
+			producer.send(new ProducerRecord(sensorTopic, builder.toString()));
+		} catch (Throwable e) {
+			//logger.warn("unable to send to kafka.",e);
+	    } finally {
 		    if (null!=producer) {
 		    	producer.close();
 		    }
-	   }
+	    }
 		
 	}
 

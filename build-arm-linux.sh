2b019126d3582863976f5a9145d30b73f88ae8a8
Brandon Sanders <brandon@alicorn.io>
2017-01-23 10:18:16 -0800

    Add linux include paths...

--- build-arm-linux.sh
+++ build-arm-linux.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "This script MUST be run on the platform you are building for with JAVA_HOME properly configured."
-gcc -I"${JAVA_HOME}/include" -I"${JAVA_HOME}/include/darwin" -shared -o src/main/resources/jni/arm-linux/rs232.so -fPIC src/main/c/RS232.c
+gcc -I"${JAVA_HOME}/include" -I"${JAVA_HOME}/include/darwin" -I"${JAVA_HOME}/include/linux" -shared -o src/main/resources/jni/arm-linux/rs232.so -fPIC src/main/c/RS232.c

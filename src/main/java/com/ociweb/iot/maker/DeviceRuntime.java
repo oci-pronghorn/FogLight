d63461a8f829958ba019b212cb7bd419590ddb96
Brandon Sanders <brandon@alicorn.io>
2017-06-01 10:23:15 -0400

    Added support for provided hardware implementations.

--- src/main/java/com/ociweb/iot/maker/DeviceRuntime.java
+++ src/main/java/com/ociweb/iot/maker/DeviceRuntime.java
@@ -23,46 +23,71 @@ import com.ociweb.pronghorn.pipe.PipeConfigManager;
 import com.ociweb.pronghorn.stage.monitor.MonitorConsoleStage;
 import com.ociweb.pronghorn.stage.scheduling.GraphManager;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class DeviceRuntime extends GreenRuntime<HardwareImpl,ListenerFilterIoT>  {
- 
+
     private static final Logger logger = LoggerFactory.getLogger(DeviceRuntime.class);
-    
 
     private final int i2cDefaultLength = 300;
     private final int i2cDefaultMaxPayload = 16;
 
     private final PipeConfig<GroveRequestSchema> requestPipeConfig = new PipeConfig<GroveRequestSchema>(GroveRequestSchema.instance, defaultCommandChannelLength);
-    private final PipeConfig<I2CCommandSchema> i2cPayloadPipeConfig = new PipeConfig<I2CCommandSchema>(I2CCommandSchema.instance, i2cDefaultLength,i2cDefaultMaxPayload);    
+    private final PipeConfig<I2CCommandSchema> i2cPayloadPipeConfig = new PipeConfig<I2CCommandSchema>(I2CCommandSchema.instance, i2cDefaultLength,i2cDefaultMaxPayload);
     private final PipeConfig<I2CResponseSchema> reponseI2CConfig = new PipeConfig<I2CResponseSchema>(I2CResponseSchema.instance, defaultCommandChannelLength, defaultCommandChannelMaxPayload);
-    private final PipeConfig<GroveResponseSchema> responsePinsConfig = new PipeConfig<GroveResponseSchema>(GroveResponseSchema.instance, defaultCommandChannelLength);      
-        
+    private final PipeConfig<GroveResponseSchema> responsePinsConfig = new PipeConfig<GroveResponseSchema>(GroveResponseSchema.instance, defaultCommandChannelLength);
+
     private static final byte piI2C = 1;
     private static final byte edI2C = 6;
-    
-    
+
+    public static final String PROVIDED_HARDWARE_IMPL_NAME = "com.ociweb.iot.hardware.impl.ProvidedHardwareImpl";
+
     public DeviceRuntime() {
-       super();     
+       super();
     }
 
 
-    
+
     public Hardware getHardware(){
     	if(this.builder==null){
-    	    
+
             ///////////////
             //setup system for binary binding in case Zulu is found on Arm
             //must populate os.arch as "arm" instead of "aarch32" or "aarch64" in that case, JIFFI is dependent on this value.
     	    if (System.getProperty("os.arch", "unknown").contains("aarch")) {
-    	        System.setProperty("os.arch", "arm"); //TODO: investigate if this a bug against jiffi or zulu and inform them 
+    	        System.setProperty("os.arch", "arm"); //TODO: investigate if this a bug against jiffi or zulu and inform them
     	    }
-    	    
-    	    
+
+    	    // Detect provided hardware implementation.
+			// TODO: Should this ONLY occur on Android devices?
+			try {
+    	    	Class.forName("android.app.Activity");
+    	    	logger.trace("Detected Android environment. Searching for {}.", PROVIDED_HARDWARE_IMPL_NAME);
+
+				try {
+					Class<?> clazz = Class.forName(PROVIDED_HARDWARE_IMPL_NAME);
+					logger.trace("Detected {}.", PROVIDED_HARDWARE_IMPL_NAME);
+					try {
+						this.builder = (HardwareImpl) clazz.getConstructor(GraphManager.class).newInstance(gm);
+						return this.builder;
+					} catch (NoSuchMethodException e) {
+						logger.warn(
+								"{} does not provide a single argument constructor that accepts a GraphManager. Continuing native hardware detection.", PROVIDED_HARDWARE_IMPL_NAME);
+					} catch (ClassCastException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+						logger.warn(
+								"Unable to instantiate {}. Continuing native hardware detection.", PROVIDED_HARDWARE_IMPL_NAME, e);
+					}
+				} catch (ClassNotFoundException e) {
+					logger.trace("No {} is present.", PROVIDED_HARDWARE_IMPL_NAME);
+				}
+			} catch (ClassNotFoundException ignored) { }
+
     	    ////////////////////////
     	    //The best way to detect the pi or edison is to first check for the expected matching i2c implmentation
     	    ///////////////////////
-    	    
+
     	    I2CBacking i2cBacking = null;
-    	    
+
     	    i2cBacking = HardwareImpl.getI2CBacking(edI2C);
 	        if (null != i2cBacking) {
 	            this.builder = new GroveV3EdisonImpl(gm, i2cBacking);
@@ -76,54 +101,54 @@ public class DeviceRuntime extends GreenRuntime<HardwareImpl,ListenerFilterIoT> 
     	        else {
     	            this.builder = new TestHardware(gm);
     	            logger.trace("Unrecognized hardware, test mock hardware will be used");
-    	        }    	        
-    	    }  	    
-    	    
+    	        }
+    	    }
+
     	}
     	return this.builder;
     }
-    
-    
 
-    
-    public CommandChannel newCommandChannel(int features) { 
-      
+
+
+
+    public CommandChannel newCommandChannel(int features) {
+
     	int instance = -1;
-    	
+
     	PipeConfigManager pcm = new PipeConfigManager();
     	pcm.addConfig(requestPipeConfig);
     	pcm.addConfig(i2cPayloadPipeConfig);
     	pcm.addConfig(defaultCommandChannelLength,0,TrafficOrderSchema.class );
-    	
-    	
+
+
     	return this.builder.newCommandChannel(features, instance, pcm);
-    	
+
     }
 
-    public CommandChannel newCommandChannel(int features, int customChannelLength) { 
-       
+    public CommandChannel newCommandChannel(int features, int customChannelLength) {
+
     	int instance = -1;
-    	
+
     	PipeConfigManager pcm = new PipeConfigManager();
     	pcm.addConfig(customChannelLength,0,GroveRequestSchema.class);
     	pcm.addConfig(customChannelLength, defaultCommandChannelMaxPayload,I2CCommandSchema.class);
     	pcm.addConfig(customChannelLength, defaultCommandChannelMaxPayload, MessagePubSub.class );
     	pcm.addConfig(customChannelLength,0,TrafficOrderSchema.class);
-    	    	
-        return this.builder.newCommandChannel(features, instance, pcm);
-        
-    }
-    
 
-    
+        return this.builder.newCommandChannel(features, instance, pcm);
+
+    }
+
+
+
     public ListenerFilterIoT addRotaryListener(RotaryListener listener) {
         return registerListener(listener);
     }
-        
+
     public ListenerFilterIoT addAnalogListener(AnalogListener listener) {
         return registerListener(listener);
     }
-    
+
     public ListenerFilterIoT addDigitalListener(DigitalListener listener) {
         return registerListener(listener);
     }
@@ -142,46 +167,46 @@ public class DeviceRuntime extends GreenRuntime<HardwareImpl,ListenerFilterIoT> 
                                                         "] hardware");
         }
     }
-        
+
     public ListenerFilterIoT addI2CListener(I2CListener listener) {
         return registerListenerImpl(listener);
     }
-    
-    
+
+
     protected ListenerFilterIoT registerListenerImpl(Object listener, int ... optionalInts) {
-        
+
     	extractPipeData(listener, optionalInts);
-    	
+
     	/////////
     	//pre-count how many pipes will be needed so the array can be built to the right size
     	/////////
     	int pipesCount = 0;
     	if (this.builder.isListeningToI2C(listener) && this.builder.hasI2CInputs()) {
-    		pipesCount++;      
+    		pipesCount++;
         }
         if (this.builder.isListeningToPins(listener) && this.builder.hasDigitalOrAnalogInputs()) {
         	pipesCount++;
         }
-        
+
         pipesCount = addGreenPipesCount(listener, pipesCount);
-        
+
         Pipe<?>[] inputPipes = new Pipe<?>[pipesCount];
-    	 	
-    	
+
+
     	///////
         //Populate the inputPipes array with the required pipes
-    	///////      
-        
-        
+    	///////
+
+
         if (this.builder.isListeningToI2C(listener) && this.builder.hasI2CInputs()) {
-        	inputPipes[--pipesCount] = (new Pipe<I2CResponseSchema>(reponseI2CConfig.grow2x()));   //must double since used by splitter         
+        	inputPipes[--pipesCount] = (new Pipe<I2CResponseSchema>(reponseI2CConfig.grow2x()));   //must double since used by splitter
         }
         if (this.builder.isListeningToPins(listener) && this.builder.hasDigitalOrAnalogInputs()) {
         	inputPipes[--pipesCount] = (new Pipe<GroveResponseSchema>(responsePinsConfig.grow2x()));  //must double since used by splitter
         }
-        
-        populateGreenPipes(listener, pipesCount, inputPipes); 
-        
+
+        populateGreenPipes(listener, pipesCount, inputPipes);
+
         /////////////////////
         //StartupListener is not driven by any response data and is called when the stage is started up. no pipe needed.
         /////////////////////
@@ -190,7 +215,7 @@ public class DeviceRuntime extends GreenRuntime<HardwareImpl,ListenerFilterIoT> 
 
         ReactiveListenerStageIOT reactiveListener = builder.createReactiveListener(gm, listener, inputPipes, outputPipes);
 		configureStageRate(listener,reactiveListener);
-        
+
 		int testId = -1;
 		int i = inputPipes.length;
 		while (--i>=0) {
@@ -198,14 +223,14 @@ public class DeviceRuntime extends GreenRuntime<HardwareImpl,ListenerFilterIoT> 
 				testId = inputPipes[i].id;
 			}
 		}
-		
+
 		assert(-1==testId || GraphManager.allPipesOfType(gm, MessageSubscription.instance)[subscriptionPipeIdx-1].id==testId) : "GraphManager has returned the pipes out of the expected order";
         return reactiveListener;
-        
+
     }
 
-        
-    public static DeviceRuntime test(IoTSetup app) {        
+
+    public static DeviceRuntime test(IoTSetup app) {
         DeviceRuntime runtime = new DeviceRuntime();
         //force hardware to TestHardware regardless of where or what platform its run on.
         //this is done because this is the test() method and must behave the same everywhere.
@@ -214,21 +239,21 @@ public class DeviceRuntime extends GreenRuntime<HardwareImpl,ListenerFilterIoT> 
         hardware.isInUnitTest = true;
         try {
         	app.declareConfiguration(runtime.builder);
-            GraphManager.addDefaultNota(runtime.gm, GraphManager.SCHEDULE_RATE, runtime.builder.getDefaultSleepRateNS()); 
-            
+            GraphManager.addDefaultNota(runtime.gm, GraphManager.SCHEDULE_RATE, runtime.builder.getDefaultSleepRateNS());
+
             runtime.declareBehavior(app);
-            
+
             runtime.builder.coldSetup(); //TODO: should we add LCD init in the PI hardware code? How do we know when its used?
-			
+
 				runtime.builder.buildStages(runtime.subscriptionPipeLookup, runtime.netPipeLookup, runtime.gm);
-				
+
 				   runtime.logStageScheduleRates();
-				
-				   if ( runtime.builder.isTelemetryEnabled()) {	   
+
+				   if ( runtime.builder.isTelemetryEnabled()) {
 					   MonitorConsoleStage.attach(runtime.gm);//documents what was buit.
 				   }
-			   //exportGraphDotFile();      
-			
+			   //exportGraphDotFile();
+
 			   runtime.scheduler = runtime.builder.createScheduler(runtime);
             //for test we do not call startup and wait instead for this to be done by test.
         } catch (Throwable t) {
@@ -237,27 +262,27 @@ public class DeviceRuntime extends GreenRuntime<HardwareImpl,ListenerFilterIoT> 
         }
         return runtime;
     }
-    
+
 	public static DeviceRuntime run(IoTSetup app) {
 	    DeviceRuntime runtime = new DeviceRuntime();
         try {
         	app.declareConfiguration(runtime.getHardware());
 		    GraphManager.addDefaultNota(runtime.gm, GraphManager.SCHEDULE_RATE, runtime.builder.getDefaultSleepRateNS());
-          
+
 		    runtime.declareBehavior(app);
-            
+
 		    System.out.println("To exit app press Ctrl-C");
 		    runtime.builder.coldSetup(); //TODO: should we add LCD init in the PI hardware code? How do we know when its used?
-			
+
 				runtime.builder.buildStages(runtime.subscriptionPipeLookup, runtime.netPipeLookup, runtime.gm);
-				
+
 				   runtime.logStageScheduleRates();
-				
-				   if ( runtime.builder.isTelemetryEnabled()) {	   
+
+				   if ( runtime.builder.isTelemetryEnabled()) {
 					   MonitorConsoleStage.attach(runtime.gm);//documents what was buit.
 				   }
-			   //exportGraphDotFile();      
-			
+			   //exportGraphDotFile();
+
 			   runtime.scheduler = runtime.builder.createScheduler(runtime);
 		    runtime.scheduler.startup();
 		} catch (Throwable t) {
@@ -267,5 +292,5 @@ public class DeviceRuntime extends GreenRuntime<HardwareImpl,ListenerFilterIoT> 
 		return runtime;
     }
 
-    
+
 }

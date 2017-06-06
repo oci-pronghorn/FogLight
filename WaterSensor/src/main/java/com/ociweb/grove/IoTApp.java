ad0da67a604065343814eccafecf80d9e1cb056e
ray-lo <raylo@go.wustl.edu>
2017-06-06 14:36:41 -0500

    water sensor now prints analog value to console

--- WaterSensor/src/main/java/com/ociweb/grove/IoTApp.java
+++ WaterSensor/src/main/java/com/ociweb/grove/IoTApp.java
@@ -7,7 +7,11 @@ import com.ociweb.iot.maker.Hardware;
 import com.ociweb.iot.maker.CommandChannel;
 import com.ociweb.iot.maker.DeviceRuntime;
 import com.ociweb.iot.maker.IoTSetup;
+import com.ociweb.iot.maker.Port;
+
 import static com.ociweb.iot.maker.Port.*;
+
+import com.ociweb.gl.api.GreenCommandChannel;
 
 public class IoTApp implements IoTSetup
 {
@@ -15,25 +19,15 @@ public class IoTApp implements IoTSetup
     //Connection constants 
     ///////////////////////
     // // by using constants such as these you can easily use the right value to reference where the sensor was plugged in
-      
-    //private static final Port BUTTON_PORT = D3;
-	//private static final Port LED_PORT    = D4;
-    //private static final Port RELAY_PORT  = D7;
-    //private static final Port LIGHT_SENSOR_PORT= A2;
-
+	
+	private static final Port WATER_SENSOR_PORT = A1;
     @Override
     public void declareConnections(Hardware c) {
         ////////////////////////////
         //Connection specifications
         ///////////////////////////
-        
-        // // specify each of the connections on the harware, eg which component is plugged into which connection.        
-              
-        //c.connect(Button, BUTTON_PORT); 
-        //c.connect(Relay, RELAY_PORT);         
-        //c.connect(LightSensor, LIGHT_SENSOR_PORT); 
-        //c.connect(LED, LED_PORT);        
-        //c.useI2C();
+    	
+        c.connect(WaterSensor,WATER_SENSOR_PORT);
         
     }
 
@@ -61,7 +55,9 @@ public class IoTApp implements IoTSetup
         //                channel1.digitalBlock(RELAY_PORT, 1000); 
         //            }
         //        });
+    	final CommandChannel channel1 = runtime.newCommandChannel(GreenCommandChannel.DYNAMIC_MESSAGING);
+    	runtime.addAnalogListener((port, time, durationMillis, average, value)->{
+    		System.out.println(value);	
+    	});
     }
-        
-  
 }

2e56d71f03a16e72de39577e3654c1335722b95c
ray-lo <raylo@go.wustl.edu>
2017-06-07 09:52:18 -0500

    cleaned up commented out template code in thumbJS

--- ThumbJoystick/src/main/java/com/ociweb/grove/IoTApp.java
+++ ThumbJoystick/src/main/java/com/ociweb/grove/IoTApp.java
@@ -20,8 +20,6 @@ public class IoTApp implements IoTSetup
 	///////////////////////
 	// // by using constants such as these you can easily use the right value to reference where the sensor was plugged in
 
-	//private static final Port BUTTON_PORT = D3;
-
 	private static final Port THUMBJOYSTICK_PORT_Y = A0;
 	private static final Port THUMBJOYSTICK_PORT_X = A1;
 
@@ -33,11 +31,7 @@ public class IoTApp implements IoTSetup
 
 		// // specify each of the connections on the harware, eg which component is plugged into which connection.        
 
-		//c.connect(Button, BUTTON_PORT); 
-		//c.connect(Relay, RELAY_PORT);         
-		//c.connect(LightSensor, LIGHT_SENSOR_PORT); 
-		//c.connect(LED, LED_PORT);        
-		//c.useI2C();
+
 		c.connect(ThumbJoystick, THUMBJOYSTICK_PORT_Y);
 		c.connect(ThumbJoystick, THUMBJOYSTICK_PORT_X);
 	}
@@ -45,27 +39,6 @@ public class IoTApp implements IoTSetup
 
 	@Override
 	public void declareBehavior(DeviceRuntime runtime) {
-		//////////////////////////////
-		//Specify the desired behavior
-		//////////////////////////////
-
-		//  //Use lambdas or classes and add listeners to the runtime object
-		//  //CommandChannels are created to send outgoing events to the hardware
-		//  //CommandChannels must never be shared between two lambdas or classes.
-		//  //A single lambda or class can use mulitiple CommandChannels for cuoncurrent behavior
-
-
-		//        final CommandChannel channel1 = runtime.newCommandChannel();
-		//        //this digital listener will get all the button press and un-press events 
-		//        runtime.addDigitalListener((connection, time, value)->{ 
-		//            
-		//            //connection could be checked but unnecessary since we only have 1 digital source
-		//            
-		//            if (channel1.digitalSetValue(RELAY_PORT, value)) {
-		//                //keep the relay on or off for 1 second before doing next command
-		//                channel1.digitalBlock(RELAY_PORT, 1000); 
-		//            }
-		//        });
 		final CommandChannel channel1 = runtime.newCommandChannel(GreenCommandChannel.DYNAMIC_MESSAGING);
 		runtime.addAnalogListener((port, time, durationMillis, average, value)->{
 			switch (port){

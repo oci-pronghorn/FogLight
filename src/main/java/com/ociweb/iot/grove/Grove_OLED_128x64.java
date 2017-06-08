96cc5b13b0ef32723281d86bf89d7b940d824975
ray-lo <raylo@go.wustl.edu>
2017-06-08 14:55:10 -0500

    added command constant for the 0.96 inch screen

--- src/main/java/com/ociweb/iot/grove/Grove_OLED_128x64.java
+++ src/main/java/com/ociweb/iot/grove/Grove_OLED_128x64.java
@@ -1,5 +1,88 @@
 package com.ociweb.iot.grove;
 
-public class Grove_OLED_128x64 {
+import com.ociweb.iot.hardware.I2CConnection;
+import com.ociweb.iot.hardware.IODevice;
+import com.ociweb.iot.maker.CommandChannel;
+import com.ociweb.pronghorn.iot.schema.I2CCommandSchema;
+import com.ociweb.pronghorn.pipe.DataOutputBlobWriter;
+
+public class Grove_OLED_128x64 implements IODevice{
+
+	
+	
+	//Overloading the function to automatically mask ints and use their least significant 8-bits as our bytes to send
+	//Ideally, for best performance, we should send byte array and not int array to avoid this extra function call
+	private static boolean writeByteSequence(CommandChannel ch, int address, int[] seq){
+		byte[] byteSeq = new byte[seq.length];
+		int counter = 0;
+		for (int i: seq){
+			byteSeq[counter] = (byte)(i & 0xFF); //this mask turns anything but the smallest 8 bits to 0
+			counter++;
+		}
+		return writeByteSequence(ch, address, byteSeq);
+	}
+	
+	private static boolean writeByteSequence(CommandChannel ch, int address, byte[] seq){
+		if(!ch.i2cIsReady()){
+			return false;
+		}
+		DataOutputBlobWriter<I2CCommandSchema> i2cPayloadWriter = ch.i2cCommandOpen(address);
+		for (byte b: seq){
+			
+			//TODO: Do we need to send to a register or address or is that logic taken care of at a lower level?
+			i2cPayloadWriter.writeByte(b);
+		}
+		ch.i2cCommandClose();
+		return true;
+	}
+	@Override
+	public int response() {
+		return 20;
+	}
+
+	@Override
+	public int scanDelay() {
+		return 0;
+	}
+
+	@Override
+	public boolean isInput() {
+		return false;
+	}
+
+	@Override
+	public boolean isOutput() {
+		return true;
+	}
+
+	@Override
+	public boolean isPWM() {
+		return false;
+	}
+
+	@Override
+	public int range() {
+		return 0;
+	}
+
+	@Override
+	public I2CConnection getI2CConnection() {
+		byte[] LED_READCMD = {};
+		byte[] LED_SETUP = {};
+		byte LED_ADDR = 0x04;
+		byte LED_BYTESTOREAD = 0;
+		byte LED_REGISTER = 0;
+		return new I2CConnection(this, LED_ADDR, LED_READCMD, LED_BYTESTOREAD, LED_REGISTER, LED_SETUP);
+	}
+
+	@Override
+	public boolean isValid(byte[] backing, int position, int length, int mask) {
+		return true;
+	}
+
+	@Override
+	public int pinsUsed() {
+		return 1;
+	}
 
 }
--- src/main/java/com/ociweb/iot/grove/Grove_OLED_128x64_Constants.java
+++ src/main/java/com/ociweb/iot/grove/Grove_OLED_128x64_Constants.java
@@ -1,46 +1,62 @@
 package com.ociweb.iot.grove;
 
 public class Grove_OLED_128x64_Constants {
+	
+	/*
+	 * NOTE: The bit-mask is there because Java does not have unsigned bytes. 
+	 * Hence, it won't consider 0x80 (0d128) for instance, to be a byte as 
+	 * it is too large for a signed byte.
+	 */
+	
 	//commands
 	
 	//these commands set a value for a particular parameter (i.e. brightness) take up
 	//a range of values (i.e. 0x00 through 0x0f). The base (lowest valued) command is listed as a consant here.
 	
-	public static int LOWER_COL_START_ADDRESS_PAGE_MODE = 0x00;//0x00 ~ 0x0f
-	public static int HIGH_COL_START_ADDRESS_PAGE_MODE = 0x10; //0x10 ~ 0x1f
-	public static int DISPLAY_START_LINE = 0x40; //40~7f
-	public static int PAGE_START_ADDRESS_PAGE_MODE = 0xB0;//0xB0 ~ 0x B7
+	public static byte LOWER_COL_START_ADDRESS_PAGE_MODE = (byte)(0x00 & 0xFF);//0x00 ~ 0x0f
+	public static byte HIGH_COL_START_ADDRESS_PAGE_MODE = (byte)(0x10 & 0xFF); //0x10 ~ 0x1f
+	public static byte DISPLAY_START_LINE = (byte)(0x40 & 0xFF); //40~7f
+	public static byte PAGE_START_ADDRESS_PAGE_MODE = (byte)(0xB0 & 0xFF);//0xB0 ~ 0x B7
 	
 	//these commands require further input afterwards
-	public static int SET_MEMORY = 0x20;
-	public static int SET_COLUMN_ADDRESS = 0x21;
-	public static int SET_PAGE_ADDRESS = 0x22;
-	public static int SET_CONTRAST_CONTROL= 0x81;
-	public static int SET_MUX_RATIO = 0xA8;
-	public static int SET_EXT_INT_I_REF = 0xAD;
-	public static int SET_DISPLAY_OFFSET = 0xD3;
-	public static int SET_CLOCK_DIV_RATIO = 0xD5;
-	public static int SET_PRECHARGE_PERIOD = 0xD9;
-	public static int SET_COM_PINS_CONFIG = 0xDA;
-	public static int SET_VCOM_DESELECT_LEVEL = 0xDB;
+	public static byte SET_MEMORY = (byte)(0x20 & 0xFF);
+	public static byte SET_COLUMN_ADDRESS = (byte)(0x21& 0xFF);
+	public static byte SET_PAGE_ADDRESS = (byte)(0x22& 0xFF);
+	public static byte SET_CONTRAST_CONTROL= (byte)(0x81 & 0xFF);
+	public static byte SET_MUX_RATIO = (byte)(0xA8 & 0xFF);
+	public static byte SET_EXT_byte_I_REF = (byte)(0xAD & 0xFF);
+	public static byte SET_DISPLAY_OFFSET = (byte)(0xD3 & 0xFF);
+	public static byte SET_CLOCK_DIV_RATIO = (byte)(0xD5 & 0xFF);
+	public static byte SET_PRECHARGE_PERIOD = (byte)(0xD9 & 0xFF);
+	public static byte SET_COM_PINS_CONFIG = (byte)(0xDA & 0xFF);
+	public static byte SET_VCOM_DESELECT_LEVEL = (byte)(0xDB & 0xFF);
+	
+	public static byte SET_RIGHT_HOR_SCROLL = (byte)(0x26 & 0xFF);
+	public static byte SET_LEFT_HOR_SCROLL = (byte)(0x27 & 0xFF);
+	public static byte SET_VER_AND_RIGHT_HOR_SCROLL = (byte)(0x29 & 0xFF);
+	public static byte SET_VER_AND_LEFT_HOR_SCROLL = (byte)(0x2A & 0xFF);
+	public static byte SET_VER_SCROLL_AREA = (byte)(0xA3 & 0xFF);
 	
 	
 	//these commands constitute a complete action on their own
 	
-	public static int MAP_ADDRESS_0_TO_SEG0 = 0xA0;
-	public static int MAP_ADDRESS_127_TO_SEG0 = 0xA1;
+	public static byte MAP_ADDRESS_0_TO_SEG0 = (byte)(0xA0 & 0xFF);
+	public static byte MAP_ADDRESS_127_TO_SEG0 = (byte)(0xA1 & 0xFF);
 	
-	public static int USE_DISPLAY_RAM = 0xA4;
-	public static int IGNORE_DISPLAY_RAM = 0xA5;
+	public static byte USE_DISPLAY_RAM = (byte)(0xA4 & 0xFF);
+	public static byte IGNORE_DISPLAY_RAM = (byte)(0xA5 & 0xFF);
 	
-	public static int TURN_ON_INVERSE_DISPLAY =  0xA6;
-	public static int TURN_OFF_INVERSE_DISPLAY = 0xA7;
+	public static byte TURN_ON_INVERSE_DISPLAY =  (byte)(0xA6 & 0xFF);
+	public static byte TURN_OFF_INVERSE_DISPLAY = (byte)(0xA7 & 0xFF);
 	
-	public static int PUT_DISPLAY_TO_SLEEP = 0xAE;
-	public static int WAKE_DISPLAY = 0xAF;
+	public static byte PUT_DISPLAY_TO_SLEEP = (byte)(0xAE & 0xFF);
+	public static byte WAKE_DISPLAY = (byte)(0xAF & 0xFF);
 	
-	public static int SET_COM_OUTPUT_SCAN_FWD = 0xC0;
-	public static int SET_COM_OUTPUT_SCAN_BKWD = 0xC8;
+	public static byte SET_COM_OUTPUT_SCAN_FWD = (byte)(0xC0 & 0xFF);
+	public static byte SET_COM_OUTPUT_SCAN_BKWD = (byte)(0xC8 & 0xFF);
 	
-	public static int NOP = 0xE3;
+	public static byte DEACTIVATE_SCROLL = (byte)(0x2E & 0xFF);
+	public static byte ACTIVATE_SCROLL = (byte)(0x2F & 0xFF);
+	
+	public static byte NOP = (byte)(0xE3 & 0xFF);
 }

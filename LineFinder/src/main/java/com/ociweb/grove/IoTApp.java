595d32dba219ec49381198d90aafca137ff55e41
huydo <huydo@172.16.10.213>
2017-06-13 13:22:23 -0500

    updated code after updated setValue() method

--- LineFinder/src/main/java/com/ociweb/grove/IoTApp.java
+++ LineFinder/src/main/java/com/ociweb/grove/IoTApp.java
@@ -1,24 +1,17 @@
 package com.ociweb.grove;
 
-
 import static com.ociweb.iot.grove.GroveTwig.*;
 
-import com.ociweb.iot.maker.CommandChannel;
-import com.ociweb.iot.maker.DeviceRuntime;
-import com.ociweb.iot.maker.Hardware;
-import com.ociweb.iot.maker.IoTSetup;
-import com.ociweb.iot.maker.Port;
+import com.ociweb.iot.maker.*;
 
 import static com.ociweb.iot.maker.Port.*;
 
-import com.ociweb.gl.api.GreenCommandChannel;
 
 
 public class IoTApp implements IoTSetup {
            
-	public static Port LED_PORT = D4;
-        public static Port LineFinderPort = D3;
-        private final int LED_HIGH = LED.range()-1;       
+	private static final Port LED_PORT = D4;
+        private static final Port LINEFINDER_PORT = D3;
         
     public static void main( String[] args) {
         DeviceRuntime.run(new IoTApp());
@@ -27,17 +20,16 @@ public class IoTApp implements IoTSetup {
     @Override
     public void declareConnections(Hardware hardware) {        
         hardware.connect(LED, LED_PORT);
-        hardware.connect(LineFinder, LineFinderPort);
+        hardware.connect(LineFinder, LINEFINDER_PORT);        
     }
 
     @Override
     public void declareBehavior(DeviceRuntime runtime) {
         
-        final CommandChannel ledChannel = runtime.newCommandChannel(GreenCommandChannel.DYNAMIC_MESSAGING); 
+        final CommandChannel ledChannel = runtime.newCommandChannel(DYNAMIC_MESSAGING); 
         runtime.addDigitalListener((port,time,durationMillis, value)->{
-                ledChannel.setValue(LED_PORT,value*LED_HIGH);
-                System.out.println("In/Out of Line");
-                    	        	
+                ledChannel.setValue(LED_PORT,value==1);
+                System.out.println("In/Out of Line");                
         });
               
     }

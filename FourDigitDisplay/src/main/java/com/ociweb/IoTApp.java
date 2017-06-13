7b45c7631d30511ee8d843fa63a9f1723564e418
ray-lo <raylo@go.wustl.edu>
2017-06-13 16:55:09 -0500

    debugging 1 input 4 outputs exception

--- FourDigitDisplay/src/main/java/com/ociweb/IoTApp.java
+++ FourDigitDisplay/src/main/java/com/ociweb/IoTApp.java
@@ -5,39 +5,37 @@ import static com.ociweb.iot.grove.GroveTwig.*;
 import com.ociweb.iot.maker.*;
 import static com.ociweb.iot.maker.Port.*;
 
+import com.ociweb.iot.grove.Grove_FourDigitDisplay;
 import com.ociweb.iot.grove.display.FourDigitDisplay;
 import com.ociweb.gl.api.GreenCommandChannel;
 
 public class IoTApp implements IoTSetup
 {
-	private static final Port CLOCK = D2;
-	private static final Port DATA = D3;
+	private static final Port CLOCK = D5;
+	private static final Port DATA = D6;
 
 	@Override
 	public void declareConnections(Hardware c) {
 	//	c.connect(FourDigitDisplay, CLOCK);
-		//c.connect(FourDigitDisplay, DATA);
+	//	c.connect(FourDigitDisplay, DATA);
+		c.connect(Button, D2,5000,false);
+		c.enableTelemetry(true);
 
 	}
-	public static void main(String args[]){
-		System.out.println("using bool setVals");
-		DeviceRuntime.run(new IoTApp());
-	}
-	
+
 	@Override
 	public void declareBehavior(DeviceRuntime runtime) {
 
 		final CommandChannel ch = runtime.newCommandChannel(GreenCommandChannel.DYNAMIC_MESSAGING);
+		final FourDigitDisplay dis = new FourDigitDisplay(ch);
+		runtime.addDigitalListener((port, a,b, val)->{		
+			Grove_FourDigitDisplay.printDigitAt(ch, 1, 0, true);
+			Grove_FourDigitDisplay.printDigitAt(ch, 2, 1, true);
+			Grove_FourDigitDisplay.printDigitAt(ch, 3, 2, true);
+			Grove_FourDigitDisplay.printDigitAt(ch, 4, 3, true);
+			
+		}).includePorts(D2);
 		
-		
-		//TODO: abstract the object dynamic allocation ("new") away from the maker
-		FourDigitDisplay dis = new FourDigitDisplay(ch);
-		dis.printDigitAt(1, 0);
-		dis.printDigitAt(2, 1);
-		dis.printDigitAt(3, 2);
-		dis.printDigitAt(1, 3);
-		
-
 	}
 
 

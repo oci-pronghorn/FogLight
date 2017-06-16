02943e9923f7671a2809effd2f1b5caae4060151
ray-lo <raylo@go.wustl.edu>
2017-06-16 12:10:37 -0500

    reverted back to 4 digit code, was using it to test oled in the mean time

--- FourDigitDisplay/src/main/java/com/ociweb/IoTApp.java
+++ FourDigitDisplay/src/main/java/com/ociweb/IoTApp.java
@@ -6,35 +6,34 @@ import com.ociweb.iot.maker.*;
 import static com.ociweb.iot.maker.Port.*;
 
 import com.ociweb.iot.grove.Grove_FourDigitDisplay;
-import com.ociweb.iot.grove.display.FourDigitDisplay;
+import static com.ociweb.iot.grove.Grove_FourDigitDisplay.*;
 import com.ociweb.gl.api.GreenCommandChannel;
 
 public class IoTApp implements FogApp
 {
-	private static final Port CLOCK = D5;
-	private static final Port DATA = D6;
+	//private static final Port CLOCK = D5;
+	//private static final Port DATA = D6;
 
 	@Override
 	public void declareConnections(Hardware c) {
 		c.connect(FourDigitDisplay, CLOCK);
 		c.connect(FourDigitDisplay, DATA);
 		c.connect(Button, D2,50,false);
+	
 
 	}
 
 	@Override
 	public void declareBehavior(FogRuntime runtime) {
 
-		final FogCommandChannel ch = runtime.newCommandChannel(GreenCommandChannel.DYNAMIC_MESSAGING);
-		final FourDigitDisplay dis = new FourDigitDisplay(ch);
+		final FogCommandChannel ch = runtime.newCommandChannel(GreenCommandChannel.DYNAMIC_MESSAGING,300);
 		runtime.addDigitalListener((port, a,b, val)->{	
-			System.out.println("printing to 4-digit display!");
-			Grove_FourDigitDisplay.printDigitAt(ch, 1, 0, true);
-			Grove_FourDigitDisplay.printDigitAt(ch, 2, 1, true);
-			Grove_FourDigitDisplay.printDigitAt(ch, 3, 2, true);
-			Grove_FourDigitDisplay.printDigitAt(ch, 4, 3, true);
-			
+			if (val !=0){
+				System.out.println("Pressed");
+				//Grove_FourDigitDisplay.printDigitAt(ch, 0, 0, true);
+				Grove_FourDigitDisplay.printDigitAt(ch, 2, 0);
+			}
 		}).includePorts(D2);
-		
+
 	}
 }

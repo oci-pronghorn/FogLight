6cd189652725265d43eb78c44f4b1787d4abe29c
Nathan Tippy <nathan@tippy.name>
2017-06-16 17:18:24 -0500

    new projects for LCDRGB and OLED screens

---
+++ LCDRGB/src/main/java/com/ociweb/grove/LCDRGB.java
@@ -0,0 +1,34 @@
+package com.ociweb.grove;
+
+
+import static com.ociweb.iot.grove.GroveTwig.*;
+
+import com.ociweb.iot.maker.*;
+import static com.ociweb.iot.maker.Port.*;
+
+public class LCDRGB implements FogApp
+{
+    ///////////////////////
+    //Connection constants 
+    ///////////////////////
+
+
+    @Override
+    public void declareConnections(Hardware c) {
+        ////////////////////////////
+        //Connection specifications
+        ///////////////////////////
+
+        
+    }
+
+
+    @Override
+    public void declareBehavior(FogRuntime runtime) {
+        //////////////////////////////
+        //Specify the desired behavior
+        //////////////////////////////
+
+    }
+          
+}

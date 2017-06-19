d24a12597c18e1cceff460cdfd64b0dd9da5dc6d
ray-lo <raylo@go.wustl.edu>
2017-06-19 13:08:06 -0500

    Brightness control for the OLED now works

--- OLED128x64/src/main/java/com/ociweb/grove/OLED128x64.java
+++ OLED128x64/src/main/java/com/ociweb/grove/OLED128x64.java
@@ -6,29 +6,35 @@ import static com.ociweb.iot.grove.GroveTwig.*;
 import com.ociweb.iot.maker.*;
 import static com.ociweb.iot.maker.Port.*;
 
+import com.ociweb.iot.grove.Grove_FourDigitDisplay;
+import com.ociweb.iot.grove.Grove_OLED_128x64;
+
 public class OLED128x64 implements FogApp
 {
-    ///////////////////////
-    //Connection constants 
-    ///////////////////////
+    
 
 
     @Override
     public void declareConnections(Hardware c) {
-        ////////////////////////////
-        //Connection specifications
-        ///////////////////////////
-
-        
+        c.useI2C();
+        c.setTriggerRate(500);
+        //TODO: give warning message if trigger rate was not set and time listener is used
     }
 
 
     @Override
     public void declareBehavior(FogRuntime runtime) {
-        //////////////////////////////
-        //Specify the desired behavior
-        //////////////////////////////
-
+    	final FogCommandChannel ch = runtime.newCommandChannel();
+    	runtime.addStartupListener(()->{	
+			System.out.println("Started");
+			Grove_OLED_128x64.init(ch);
+		});
+    	runtime.addTimeListener((time,iteration) ->{
+    		int brightness = (int)(Math.sin(time/(1000.0*Math.PI)) * 255 + 255);
+    		System.out.println(brightness);	
+    		Grove_OLED_128x64.setContrast(ch, brightness);
+    	});
+    	
     }
           
 }

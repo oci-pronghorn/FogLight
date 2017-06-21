c563afdba78d10053b7d6a9da3ce0bcdbc2a066f
Nathan Tippy <nathan@tippy.name>
2017-06-21 08:56:08 -0500

    added public static main start for IDEs to find.

---
+++ src/main/resources/archetype-resources/src/main/java/FogLight.java
@@ -0,0 +1,11 @@
+package ${package};
+
+import com.ociweb.iot.maker.FogRuntime;
+
+public class FogLight {
+
+	public static void main(String[] args) {
+		FogRuntime.run(new ${artifactId}());
+	}
+	
+}

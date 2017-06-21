3dfb5ca98f703739c00f2b091ed0f3cdd0f35e66
dwaks <waksd@g.cofc.edu>
2017-06-21 09:08:54 -0500

    Update IoTApp.java

--- LED/src/main/java/com/ociweb/grove/IoTApp.java
+++ LED/src/main/java/com/ociweb/grove/IoTApp.java
@@ -16,58 +16,27 @@ import com.ociweb.gl.api.GreenCommandChannel;
 
 public class IoTApp implements FogApp
 {
-    ///////////////////////
-    //Connection constants 
-    ///////////////////////
-    // // by using constants such as these you can easily use the right value to reference where the sensor was plugged in
-      
     private static final Port BUTTON_PORT = D3;
-	private static final Port LED_PORT    = D2;
-    //private static final Port RELAY_PORT  = D7;
-    //private static final Port LIGHT_SENSOR_PORT= A2;
-
+    private static final Port LED_PORT    = D2;
+    
     @Override
     public void declareConnections(Hardware c) {
-        ////////////////////////////
-        //Connection specifications
-        ///////////////////////////
-        
-        // // specify each of the connections on the harware, eg which component is plugged into which connection.        
               
         c.connect(Button, BUTTON_PORT); 
-        //c.connect(Relay, RELAY_PORT);         
-        //c.connect(LightSensor, LIGHT_SENSOR_PORT); 
         c.connect(LED, LED_PORT);        
         c.useI2C();
-        
     }
 
 
     @Override
     public void declareBehavior(FogRuntime runtime) {
-        //////////////////////////////
-        //Specify the desired behavior
-        //////////////////////////////
-        
-        //  //Use lambdas or classes and add listeners to the runtime object
-        //  //CommandChannels are created to send outgoing events to the hardware
-        //  //CommandChannels must never be shared between two lambdas or classes.
-        //  //A single lambda or class can use mulitiple CommandChannels for cuoncurrent behavior
-        
-        
-                final FogCommandChannel channel1 = runtime.newCommandChannel(GreenCommandChannel.DYNAMIC_MESSAGING);
-               //this digital listener will get all the button press and un-press events 
-                runtime.addDigitalListener((port, connection, time, value)->{ 
-        //            
-        //            //connection could be checked but unnecessary since we only have 1 digital source
-        //            
-                    if (BUTTON_PORT == port){
-                    	channel1.setValue(LED_PORT, value); 
-        //                //keep the relay on or off for 1 second before doing next command
-        //                channel1.digitalBlock(RELAY_PORT, 1000); 
-                    }
-               });
+           
+    	final FogCommandChannel channel1 = runtime.newCommandChannel(GreenCommandChannel.DYNAMIC_MESSAGING);
+        //this digital listener will get all the button press and un-press events 
+        runtime.addDigitalListener((port, connection, time, value)->{
+        	if (BUTTON_PORT == port){
+                	channel1.setValueAndBlock(LED_PORT, value, 200); 
+                }
+        });
     }
-        
-  
 }

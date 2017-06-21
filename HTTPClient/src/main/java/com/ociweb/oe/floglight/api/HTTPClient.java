0c5ac3bc9e05cd936ee399adc5a4d5d447f4be98
Nathan Tippy <nathan@tippy.name>
2017-06-21 09:25:02 -0500

    Adding first empty API projects

---
+++ HTTPClient/src/main/java/com/ociweb/oe/floglight/api/HTTPClient.java
@@ -0,0 +1,34 @@
+package com.ociweb.oe.floglight.api;
+
+
+import static com.ociweb.iot.grove.GroveTwig.*;
+
+import com.ociweb.iot.maker.*;
+import static com.ociweb.iot.maker.Port.*;
+
+public class HTTPClient implements FogApp
+{
+    ///////////////////////
+    //Connection constants 
+    ///////////////////////
+
+
+    @Override
+    public void declareConnections(Hardware c) {
+        ////////////////////////////
+        //Connection specifications
+        ///////////////////////////
+
+        
+    }
+
+
+    @Override
+    public void declareBehavior(FogRuntime runtime) {
+        //////////////////////////////
+        //Specify the desired behavior
+        //////////////////////////////
+
+    }
+          
+}
---
+++ HTTPServer/src/main/java/com/ociweb/oe/foglight/api/FogLight.java
@@ -0,0 +1,11 @@
+package com.ociweb.oe.foglight.api;
+
+import com.ociweb.iot.maker.FogRuntime;
+
+public class FogLight {
+
+	public static void main(String[] args) {
+		FogRuntime.run(new HTTPServer());
+	}
+	
+}

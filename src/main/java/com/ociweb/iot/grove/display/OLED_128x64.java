68db3cd715421bca7511fc53e472f0d5b96bda41
ray-lo <raylo@go.wustl.edu>
2017-06-22 15:03:12 -0500

    object layer of 128x64 implemented, minimized garbage in static layer for both commands and data transmission

---
+++ src/main/java/com/ociweb/iot/grove/display/OLED_128x64.java
@@ -0,0 +1,114 @@
+package com.ociweb.iot.grove.display;
+
+import static com.ociweb.iot.grove.Grove_OLED_128x64_Constants.col_count;
+import static com.ociweb.iot.grove.Grove_OLED_128x64_Constants.row_count;
+
+import com.ociweb.iot.grove.Grove_OLED_128x64;
+import com.ociweb.iot.hardware.I2CConnection;
+import com.ociweb.iot.hardware.IODevice;
+import com.ociweb.iot.maker.FogCommandChannel;
+
+public class OLED_128x64 implements IODevice{
+
+	FogCommandChannel ch;
+	
+	int[] data_output = new int[1024]; //the most amount of data we can ever send at once as this is one entire frame worth of data
+	int[] cmd_output = new int[32]; // the static Grove_OLED_128x64 class requires that we send out no more than 10 bytes at once. 32 bytes are allocated for safety.
+	
+	public OLED_128x64(FogCommandChannel ch){
+		this.ch = ch;
+	}
+	
+	public boolean init(){
+		return Grove_OLED_128x64.init(this.ch, cmd_output);
+	}
+	
+	public boolean clear(){
+		return Grove_OLED_128x64.clear(this.ch, cmd_output);
+	}
+	
+	public boolean cleanClear(){
+		return Grove_OLED_128x64.cleanClear(this.ch, cmd_output);
+	}
+	
+	public boolean setTextRowCol(int row, int col){
+		return Grove_OLED_128x64.setTextRowCol(this.ch, row, col, cmd_output);
+	}
+	public boolean printString(String s){
+		return Grove_OLED_128x64.printString(this.ch, s , data_output);
+	}
+	
+	public boolean printStringAt(String s, int row, int col){
+		return Grove_OLED_128x64.printStringAt(this.ch, s, data_output, row, col, cmd_output);
+	}
+	
+	public boolean displayImage(int[][] raw_image){
+		int counter = 0;
+		for (int page = 0; page < row_count/8; page++){
+			for (int seg = 0; seg < col_count; seg++){
+				data_output[counter] = parseColByte(raw_image, page*8, seg);
+				counter++;
+			}
+		}
+		return Grove_OLED_128x64.drawBitmap(ch, data_output, cmd_output);
+	}
+	
+	public boolean drawBitmap(int[] bitmap){
+		return Grove_OLED_128x64.drawBitmap(this.ch, bitmap, cmd_output);
+	}
+	
+	private static int parseColByte(int[][]raw_image, int row, int col){
+		int ret = 0;
+		for (int i = 0; i < 8; i ++){
+			ret = ret | (raw_image[row+i][col] & 0x01) << i;
+		}
+		return ret;
+	}
+	
+	
+	@Override
+	public int response() {
+		return 0;
+	}
+
+	@Override
+	public int scanDelay() {
+		return 0;
+	}
+
+	@Override
+	public boolean isInput() {
+		return false;
+	}
+
+	@Override
+	public boolean isOutput() {
+		return true;
+	}
+
+	@Override
+	public boolean isPWM() {
+		return false;
+	}
+
+	@Override
+	public int range() {
+		return 0;
+	}
+
+	@Override
+	public I2CConnection getI2CConnection() {
+		return null;
+	}
+
+	@Override
+	public boolean isValid(byte[] backing, int position, int length, int mask) {
+		return false;
+	}
+
+	@Override
+	public int pinsUsed() {
+		return 1;
+	}
+
+}

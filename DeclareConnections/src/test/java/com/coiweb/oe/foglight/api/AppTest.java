828ee69c6e3f52f33d44b07a7f5af9e9f3175cb2
Nathan Tippy <nathan@tippy.name>
2017-06-29 11:37:34 -0500

    added new API projects for declareConnections and listeners

---
+++ DeclareConnections/src/test/java/com/coiweb/oe/foglight/api/AppTest.java
@@ -0,0 +1,39 @@
+package com.coiweb.oe.foglight.api;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.ociweb.iot.hardware.impl.test.TestHardware;
+import com.ociweb.iot.maker.FogRuntime;
+import com.ociweb.pronghorn.stage.scheduling.NonThreadScheduler;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest { 
+
+	
+	 @Test
+	    public void testApp()
+	    {
+		    FogRuntime runtime = FogRuntime.test(new DeclareConnections());	    	
+	    	NonThreadScheduler scheduler = (NonThreadScheduler)runtime.getScheduler();    	
+	    	TestHardware hardware = (TestHardware)runtime.getHardware();
+	    
+	    	scheduler.startup();
+	    	
+	    	int iterations = 10;
+			while (--iterations >= 0) {
+				    		
+					scheduler.run();
+					
+					//test application here
+					
+			}
+			
+			scheduler.shutdown();
+			
+	    }
+}
---
+++ DigitalListener/src/main/java/com/coiweb/oe/foglight/api/DigitalListener.java
@@ -0,0 +1,34 @@
+package com.coiweb.oe.foglight.api;
+
+
+import static com.ociweb.iot.grove.GroveTwig.*;
+
+import com.ociweb.iot.maker.*;
+import static com.ociweb.iot.maker.Port.*;
+
+public class DigitalListener implements FogApp
+{
+    ///////////////////////
+    //Connection constants 
+    ///////////////////////
+
+
+    @Override
+    public void declareConnections(Hardware c) {
+        ////////////////////////////
+        //Connection specifications
+        ///////////////////////////
+
+        
+    }
+
+
+    @Override
+    public void declareBehavior(FogRuntime runtime) {
+        //////////////////////////////
+        //Specify the desired behavior
+        //////////////////////////////
+
+    }
+          
+}

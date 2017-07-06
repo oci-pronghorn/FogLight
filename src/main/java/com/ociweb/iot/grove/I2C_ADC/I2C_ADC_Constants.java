1e8a69e32b57ea32c9a1f0b150fe0505f6438dec
ray-lo <raylo@go.wustl.edu>
2017-07-06 15:13:43 -0500

    added back I2cADC

---
+++ src/main/java/com/ociweb/iot/grove/I2C_ADC/I2C_ADC_Constants.java
@@ -0,0 +1,27 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ociweb.iot.grove.I2C_ADC;
+
+/**
+ *
+ * @author huydo
+ */
+public class I2C_ADC_Constants {
+
+	public static final int ADDR_ADC121     =        0x50;
+
+	public static final int REG_ADDR_RESULT =        0x00;
+	public static final int REG_ADDR_ALERT  =        0x01;
+	public static final int REG_ADDR_CONFIG =        0x02;
+	public static final int REG_ADDR_LIMITL =        0x03;
+	public static final int REG_ADDR_LIMITH =        0x04;
+	public static final int REG_ADDR_HYST   =        0x05;
+	public static final int REG_ADDR_CONVL  =        0x06;
+	public static final int REG_ADDR_CONVH  =        0x07;
+
+}
+

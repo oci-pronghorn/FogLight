095036758a4b76377f4c7288fa7062e8b2a13b31
dwaks <dwaks@g.cofc.edu>
2017-07-12 14:39:28 -0500

    added markdown

--- AnalogListener/src/main/java/com/coiweb/oe/foglight/api/AnalogListener.java
+++ AnalogListener/src/main/java/com/coiweb/oe/foglight/api/AnalogListener.java
@@ -8,28 +8,18 @@ import static com.ociweb.iot.maker.Port.*;
 
 public class AnalogListener implements FogApp
 {
-    ///////////////////////
-    //Connection constants 
-    ///////////////////////
 	private static Port LIGHT_SENSOR_PORT = A2;
-
 
     @Override
     public void declareConnections(Hardware c) {
-        ////////////////////////////
-        //Connection specifications
-        ///////////////////////////
+        
     	c.connect(LightSensor, LIGHT_SENSOR_PORT);
-
         
     }
 
-
     @Override
     public void declareBehavior(FogRuntime runtime) {
-        //////////////////////////////
-        //Specify the desired behavior
-        //////////////////////////////
+        
     	runtime.addAnalogListener(new AnalogListenerBehavior(runtime)).includePorts(LIGHT_SENSOR_PORT).excludePorts(DIGITALS);
     }
           
--- AnalogListener/src/main/java/com/coiweb/oe/foglight/api/AnalogListenerBehavior.java
+++ AnalogListener/src/main/java/com/coiweb/oe/foglight/api/AnalogListenerBehavior.java
@@ -8,15 +8,13 @@ public class AnalogListenerBehavior implements AnalogListener {
 
 	
 	public AnalogListenerBehavior(FogRuntime runtime) {
-		// TODO Auto-generated constructor stub
 		
 	}
 
 	@Override
 	public void analogEvent(Port port, long time, long durationMillis, int average, int value) {
-		// TODO Auto-generated method stub
+
 		System.out.println("value: " + value);
-		
 	}
 
 }

70478b89a558b39244cf79e612e9f50d768b6e36
ray-lo <raylo@go.wustl.edu>
2017-07-12 08:39:39 -0500

    merge

--- src/main/java/com/ociweb/iot/grove/gps/GPS_Facade.java
+++ src/main/java/com/ociweb/iot/grove/gps/GPS_Facade.java
@@ -1,10 +1,20 @@
 package com.ociweb.iot.grove.gps;
 
 import com.ociweb.iot.maker.FogCommandChannel;
+import com.ociweb.iot.maker.FogRuntime;
+import com.ociweb.iot.maker.SerialListener;
+import com.ociweb.iot.maker.SerialReader;
 
-public class GPS_Facade {
-	private final FogCommandChannel ch;
-	public GPS_Facade(FogCommandChannel ch){
-		this.ch = ch;
+
+public class GPS_Facade implements SerialListener{
+	private final FogRuntime rt;
+	public GPS_Facade(FogRuntime rt){
+		this.rt = rt;
 	}
+	@Override
+	public int message(SerialReader reader) {
+		
+		return 0;
+	}
+	
 }

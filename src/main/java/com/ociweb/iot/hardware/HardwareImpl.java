30ed253b7616bf9e8c6e20c953619de7ee9edd58
Nathan Tippy <nathan@tippy.name>
2017-07-17 11:28:00 -0500

    remove extra check

--- src/main/java/com/ociweb/iot/hardware/HardwareImpl.java
+++ src/main/java/com/ociweb/iot/hardware/HardwareImpl.java
@@ -648,9 +648,9 @@ public abstract class HardwareImpl extends BuilderImpl implements Hardware {
 			assert(!isPinWriter || (IDX_PIN>0)) : "Pin feature is on but not used?";
 			assert(!isSerialWriter || (IDX_SER>0)) : "Serial feature is on but not used?";
 			assert(!isNetRequester || (IDX_NET>0)) : "Net requests feature is on but not used?";
-			assert(!isDynamicMessaging || (IDX_MSG>0)) : "PubSub feature is on but not used?";
 
 			//PI?
+			//assert(!isDynamicMessaging || (IDX_MSG>0)) : "PubSub feature is on but not used?";
 			//			assert(!isI2CWriter || (IDX_I2C>0)) : "I2C feature is on but not used?";
 						
 			boolean hasConnections = false;

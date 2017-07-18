42bf499963d39b6e1ea58bc3bc4a533f88c2214e
dwaks <waksd@g.cofc.edu>
2017-07-18 15:14:46 -0500

    Update StateMachine.java

--- StateMachine/src/main/java/com/ociweb/oe/foglight/api/StateMachine.java
+++ StateMachine/src/main/java/com/ociweb/oe/foglight/api/StateMachine.java
@@ -4,6 +4,8 @@ package com.ociweb.oe.foglight.api;
 import static com.ociweb.iot.grove.GroveTwig.*;
 
 import com.ociweb.iot.maker.*;
+import com.ociweb.oe.foglight.api.StateMachine.StopLight;
+
 import static com.ociweb.iot.maker.Port.*;
 
 import com.ociweb.gl.api.StateChangeListener;
@@ -32,56 +34,22 @@ public class StateMachine implements FogApp
 		}
 	}
 
-
+	
     @Override
     public void declareConnections(Hardware c) {
     	
-    	c.startStateMachineWith(StopLight.Go);
+    	c.startStateMachineWith(StopLight.Stop);
+    	c.setTimerPulseRate(1);
     }
 
    
-    @Override
+	@SuppressWarnings("unchecked")
+	@Override
     public void declareBehavior(FogRuntime runtime) {
 
-        final FogCommandChannel channel1 = runtime.newCommandChannel(DYNAMIC_MESSAGING);
-        final FogCommandChannel channel2 = runtime.newCommandChannel(DYNAMIC_MESSAGING);
-
         
-        
-        runtime.addStartupListener(()-> {
-        	channel2.changeStateTo(StopLight.Caution);
-        	System.out.println("first");
-        });
-        
-        
-    	StateChangeListener<StopLight> thing = (oldState, newState) -> {
-    		
-    		if(newState == StopLight.Go){
-    			System.out.println("Go! The light is " + StopLight.Go.getColor());
-    			if(channel1.changeStateTo(StopLight.Caution)){
-    				channel1.block(7000);
-    			}
-    		}
-    		
-    		else if(newState == StopLight.Caution){
-    			System.out.println("The light is " + StopLight.Caution.getColor());
-
-    			if(channel1.changeStateTo(StopLight.Stop)){
-    				channel1.block(7000);
-    			}
-    		}
-    		
-    		else{
-    			System.out.println("Stop! The light is " + StopLight.Stop.getColor());
-    			if(channel1.changeStateTo(StopLight.Go)){
-    				channel1.block(7000);
-    			}
-    		}
-    		return true;
-    	};
-    	
-		runtime.addStateChangeListener(thing);
-    	
+        runtime.addTimePulseListener(new TimingBehavior(runtime));
+		runtime.addStateChangeListener(new StateChangeBehavior());
     }
           
 }

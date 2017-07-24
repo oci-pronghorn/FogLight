96e536675da0aa0a14b83b692cae59c522520617
Nathan Tippy <nathan@tippy.name>
2017-07-24 14:01:23 -0500

    update to AnalogTransducer
    update to send args for HTTP client

--- HTTPClient/src/main/java/com/ociweb/oe/floglight/api/FogLight.java
+++ HTTPClient/src/main/java/com/ociweb/oe/floglight/api/FogLight.java
@@ -5,7 +5,7 @@ import com.ociweb.iot.maker.FogRuntime;
 public class FogLight {
 
 	public static void main(String[] args) {
-		FogRuntime.run(new HTTPClient());
+		FogRuntime.run(new HTTPClient(),args);
 	}
 	
 }

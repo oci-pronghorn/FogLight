3355525a5834d49eac2d4a4d2725b7a1c574752b
ray-lo <raylo@go.wustl.edu>
2017-07-24 15:37:28 -0500

    adding constructor

--- src/main/java/com/ociweb/iot/grove/gps/GPS_Transducer.java
+++ src/main/java/com/ociweb/iot/grove/gps/GPS_Transducer.java
@@ -1,23 +1,47 @@
 package com.ociweb.iot.grove.gps;
 
+import java.io.IOException;
+
 import com.ociweb.iot.maker.FogCommandChannel;
-import com.ociweb.iot.maker.FogRuntime;
 import com.ociweb.iot.maker.IODeviceTransducer;
-import com.ociweb.iot.maker.SerialListener;
-import com.ociweb.iot.maker.SerialReader;
+import com.ociweb.iot.transducer.SerialListenerTransducer;
 import com.ociweb.pronghorn.pipe.BlobReader;
 
 
-public class GPS_Transducer implements SerialListener, IODeviceTransducer{
+public class GPS_Transducer implements SerialListenerTransducer, IODeviceTransducer{
 	private final FogCommandChannel ch;
 	private GeoCoordinateListener l;
+	private char[] input;
+	public GPS_Transducer(FogCommandChannel ch, GeoCoordinateListener l){
+		this.l = l;
+		this.ch = ch;
+	}
+	
 	public GPS_Transducer(FogCommandChannel ch){
 		this.ch = ch;
 	}
+//	public void addGeoCoordinateListener (GeoCoordinateListener l){
+//		this.l = l;
+//	}
 	@Override
-	public int message(BlobReader reader) {		
+	public int message(BlobReader reader) {	
+		int index = 0;
+		while (reader.hasRemainingBytes()){
+			try {
+				input[index++] = reader.readChar();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		printMsg(index);
 		l.coordinates(1, 2);
-		return 0;
+		return index;
 	}
 	
+	private void printMsg(int length){
+		for (int i = 0; i < length; i ++){
+			System.out.print(input[i]);
+		}
+		System.out.println();
+	}
 }

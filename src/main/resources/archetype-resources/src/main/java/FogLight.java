3ca7107bdf4474aff02886ceb606ef51a68934a2
Nathan Tippy <nathan@tippy.name>
2017-07-25 10:04:51 -0500

    add args

--- src/main/resources/archetype-resources/src/main/java/FogLight.java
+++ src/main/resources/archetype-resources/src/main/java/FogLight.java
@@ -5,7 +5,7 @@ import com.ociweb.iot.maker.FogRuntime;
 public class FogLight {
 
 	public static void main(String[] args) {
-		FogRuntime.run(new ${artifactId}());
+		FogRuntime.run(new ${artifactId}(),args);
 	}
 	
 }

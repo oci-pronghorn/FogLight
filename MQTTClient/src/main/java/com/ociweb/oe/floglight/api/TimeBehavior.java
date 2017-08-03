27ae85a190645480197ccfc63171aae449a5caee
Nathan Tippy <nathan@tippy.name>
2017-08-03 16:39:06 -0500

    remove old interface

--- MQTTClient/src/main/java/com/ociweb/oe/floglight/api/TimeBehavior.java
+++ MQTTClient/src/main/java/com/ociweb/oe/floglight/api/TimeBehavior.java
@@ -2,9 +2,9 @@ package com.ociweb.oe.floglight.api;
 
 import java.util.Date;
 
-import com.ociweb.gl.api.PubSubWritable;
 import com.ociweb.gl.api.TimeListener;
 import com.ociweb.gl.api.WaitFor;
+import com.ociweb.gl.api.Writable;
 import com.ociweb.iot.maker.FogCommandChannel;
 import com.ociweb.iot.maker.FogRuntime;
 import com.ociweb.pronghorn.pipe.BlobWriter;
@@ -27,7 +27,7 @@ public class TimeBehavior implements TimeListener {
 			Date d =new Date(System.currentTimeMillis());
 			
 			// On the timer event create a payload with a string encoded timestamp
-			PubSubWritable writable = new PubSubWritable() {
+			Writable writable = new Writable() {
 				@Override
 				public void write(BlobWriter writer) {
 					writer.writeUTF8Text("egress body "+d);

6041e9ca6910f713b8bbe5c0eb9edab344aa53e7
ray-lo <raylo@go.wustl.edu>
2017-08-07 14:05:31 -0500

    merging

--- MP3Player/src/main/java/com/ociweb/grove/MP3Player.java
+++ MP3Player/src/main/java/com/ociweb/grove/MP3Player.java
@@ -15,20 +15,20 @@ public class MP3Player implements FogApp
 
     @Override
     public void declareConnections(Hardware c) {
-        ////////////////////////////
-        //Connection specifications
-        ///////////////////////////
-
-        
+    	c.useSerial(Baud.B_____9600);
+    	c.setTimerPulseRate(1000);
+    	//c.enableTelemetry();
+    	c.limitThreads();
     }
 
 
     @Override
     public void declareBehavior(FogRuntime runtime) {
-        //////////////////////////////
-        //Specify the desired behavior
-        //////////////////////////////
-
+    	runtime.registerListener(new MP3Behavior(runtime));
+    	runtime.registerListener(new MonitoringBehavior());
+    }
+    public static void main (String[] args){
+    	FogRuntime.run(new MP3Player());
     }
           
 }

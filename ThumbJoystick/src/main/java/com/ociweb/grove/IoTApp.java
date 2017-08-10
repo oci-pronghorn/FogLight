f2c2c5defc42a9768c60a85f912e583fd8918303
ray-lo <raylo@go.wustl.edu>
2017-08-10 14:32:20 -0500

    serial debug again

--- ThumbJoystick/src/main/java/com/ociweb/grove/IoTApp.java
+++ ThumbJoystick/src/main/java/com/ociweb/grove/IoTApp.java
@@ -9,7 +9,7 @@ import com.ociweb.gl.api.MsgCommandChannel;
 
 public class IoTApp implements FogApp
 {
-	private static final Port THUMBJOYSTICK_PORT = A1;
+	private static final Port THUMBJOYSTICK_PORT = A2;
 
 	@Override
 	public void declareConnections(Hardware c) {
--- WaterSensor/src/main/java/com/ociweb/grove/RestfulWaterSensorBehavior.java
+++ WaterSensor/src/main/java/com/ociweb/grove/RestfulWaterSensorBehavior.java
@@ -10,7 +10,7 @@ import com.ociweb.pronghorn.network.config.HTTPContentTypeDefaults;
 public class RestfulWaterSensorBehavior implements AnalogListener, RestListener  {
 	private int val = -1;
 	private FogCommandChannel ch;
-	
+
 	public RestfulWaterSensorBehavior(FogRuntime runtime) {
 		this.ch = runtime.newCommandChannel(MsgCommandChannel.NET_RESPONDER | MsgCommandChannel.DYNAMIC_MESSAGING); 
 	}	
@@ -19,21 +19,19 @@ public class RestfulWaterSensorBehavior implements AnalogListener, RestListener 
 	public boolean restRequest(HTTPRequestReader reader) {
 		return ch.publishHTTPResponse(reader, 200, HTTPFieldReader.END_OF_RESPONSE | HTTPFieldReader.CLOSE_CONNECTION, HTTPContentTypeDefaults.HTML,
 				(writer)->{
-					
-						try {
-							writer.append(Integer.toString(this.val));
-						} catch (Exception e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-				
+					try {
+						writer.append(Integer.toString(this.val));
+					} catch (Exception e) {
+
+						e.printStackTrace();
+					}		
 				});
 	}
 
 	@Override
 	public void analogEvent(Port port, long time, long durationMillis, int average, int value) {
-			System.out.println(val);
-			this.val = value;
+		System.out.println(val);
+		this.val = value;
 	}
 
 }

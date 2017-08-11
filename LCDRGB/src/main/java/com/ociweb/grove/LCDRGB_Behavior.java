be3912995cde121a6dac83af65a132919895c9da
ray-lo <raylo@go.wustl.edu>
2017-08-11 10:51:45 -0500

    hello world example done

--- LCDRGB/src/main/java/com/ociweb/grove/LCDRGB_Behavior.java
+++ LCDRGB/src/main/java/com/ociweb/grove/LCDRGB_Behavior.java
@@ -5,6 +5,7 @@ import com.ociweb.gl.api.StartupListener;
 import com.ociweb.gl.api.TimeListener;
 import com.ociweb.iot.grove.lcd_rgb.LCD_RGB_Transducer;
 import com.ociweb.iot.maker.FogCommandChannel;
+import com.ociweb.iot.maker.FogRuntime;
 
 import static com.ociweb.iot.grove.lcd_rgb.LCD_RGB_Twig.*;
 
@@ -13,14 +14,20 @@ public class LCDRGB_Behavior implements Behavior, StartupListener {
 	private LCD_RGB_Transducer lcd;
 
 	
-	public LCDRGB_Behavior(FogCommandChannel ch){
-		lcd = LCD_RGB.newTransducer(ch);
-		
+	public LCDRGB_Behavior(FogRuntime rt){
+		lcd = LCD_RGB.newTransducer(rt.newCommandChannel());	
+	}
+
+
+	@Override
+	public void startup() {
+		lcd.setCursor(0, 0);
+		lcd.commandForTextAndColor("Hello Wall!", 63, 63, 63);
 	}
 	
-	@Override
-	public void timeEvent(long time, int iteration) {
+
+	
 		
-	}
+	
 
 }

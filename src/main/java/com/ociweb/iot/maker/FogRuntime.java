63541558bf8323394851314b43c6f9755deace69
Nathan Tippy <nathan@tippy.name>
2017-09-12 07:31:59 -0500

    less CPU by lowering run frequency

--- src/main/java/com/ociweb/iot/maker/FogRuntime.java
+++ src/main/java/com/ociweb/iot/maker/FogRuntime.java
@@ -379,7 +379,7 @@ public class FogRuntime extends MsgRuntime<HardwareImpl, ListenerFilterIoT>  {
 		logger.info("{} ms startup", lastTime = System.currentTimeMillis());
 		Hardware hardware = runtime.getHardware();
 		//this default for Fog is slower due to the expected minimum hardware of iot devices
-		hardware.setDefaultRate(20_000); // 1/50 of 1 ms
+		hardware.setDefaultRate(100_000); // 1/10 of 1 ms
 
 		app.declareConfiguration(hardware);
 		GraphManager.addDefaultNota(runtime.gm, GraphManager.SCHEDULE_RATE, runtime.builder.getDefaultSleepRateNS());

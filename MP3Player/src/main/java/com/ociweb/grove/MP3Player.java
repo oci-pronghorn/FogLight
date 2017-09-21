01e65ad53ed428cad39eaac47c1418f414eb6354
Nathan Tippy <nathan@tippy.name>
2017-09-20 21:02:46 -0500

    remove bad import

--- MP3Player/src/main/java/com/ociweb/grove/MP3Player.java
+++ MP3Player/src/main/java/com/ociweb/grove/MP3Player.java
@@ -1,10 +1,10 @@
 package com.ociweb.grove;
 
 
-import com.ociweb.iot.maker.*;
-
-import static com.ociweb.iot.grove.analogdigital.AnalogDigitalTwig.*;
-import static com.ociweb.iot.maker.Port.*;
+import com.ociweb.iot.maker.Baud;
+import com.ociweb.iot.maker.FogApp;
+import com.ociweb.iot.maker.FogRuntime;
+import com.ociweb.iot.maker.Hardware;
 
 public class MP3Player implements FogApp
 {

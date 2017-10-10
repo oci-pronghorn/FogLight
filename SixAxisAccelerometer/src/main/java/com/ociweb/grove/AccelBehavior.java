29f99a4f8cb4e99c0dd90f1b7e93109fd1bb2dc2
David Giovannini <dave@David.local>
2017-10-10 14:35:53 -0500

    api change

--- SixAxisAccelerometer/src/main/java/com/ociweb/grove/AccelBehavior.java
+++ SixAxisAccelerometer/src/main/java/com/ociweb/grove/AccelBehavior.java
@@ -14,44 +14,25 @@ import com.ociweb.iot.maker.FogRuntime;
  *
  * @author huydo
  */
-public class AccelBehavior implements Behavior, AccelValsListener {
+public class AccelBehavior implements Behavior {
     private final FogCommandChannel ch;
     
     private final SixAxisAccelerometer_Transducer accSensor;
-    private final AccerometerMagValues magValues;
+    private final AccerometerValues values;
 
     AccelBehavior(FogRuntime runtime){
         this.ch = runtime.newCommandChannel();
-        this.magValues = new AccerometerMagValues(
-                AccelerometerMagDataRate.hz50,
-                AccelerometerMagScale.gauss8,
-                AccelerometerMagRes.high) {
-
+        this.values = new AccerometerValues() {
             @Override
-            public void onChange() {
-                System.out.println("heading: " + magValues.getHeading());
+            public void onChange(Changed changed) {
+                if (changed == Changed.mag) {
+                    System.out.println("heading: " + values.getHeading());
+                }
+                else if (changed == Changed.accel) {
+                    System.out.println("accel x: " + values.getAccelX() + " y: " + values.getAccelY() + " z: "+ values.getAccelZ());
+                }
             }
         };
-        accSensor = new SixAxisAccelerometer_Transducer(ch, this, magValues, null);
-    }
-
-    @Override
-    public AccelerometerAccelDataRate getAccerometerDataRate() {
-        return AccelerometerAccelDataRate.hz50;
-    }
-
-    @Override
-    public AccelerometerAccelScale getAccerometerScale() {
-        return AccelerometerAccelScale.gauss6;
-    }
-
-    @Override
-    public int getAccerometerAxes() {
-        return AccelerometerAccelAxes.all;
-    }
-
-    @Override
-    public void accelerationValues(int x, int y, int z) {
-        System.out.println("accel x: " + x + " y: " + y + " z: "+ z);
+        accSensor = new SixAxisAccelerometer_Transducer(ch, values, values, null);
     }
 }

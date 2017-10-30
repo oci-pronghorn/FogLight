09b2f597510c2e4dd82adbb9e58c06e1b0b25c17
Nathan Tippy <nathan@tippy.name>
2017-10-29 19:20:17 -0500

    remove bad comment

--- src/main/java/com/ociweb/iot/grove/six_axis_accelerometer/AccelerometerAccelDataRate.java
+++ src/main/java/com/ociweb/iot/grove/six_axis_accelerometer/AccelerometerAccelDataRate.java
@@ -2,7 +2,7 @@ package com.ociweb.iot.grove.six_axis_accelerometer;
 
 /**
  * Set accelerometer output data rate
- * @param aRate
+ * 
  * 1 = 3Hz, 2 = 6Hz, 3 =12 Hz,  4 = 25 Hz, 5 = 50Hz
  * 6 = 100 Hz, 7 = 200 Hz, 8 = 400 Hz
  * 9 = 800 Hz, 10 = 1600 Hz

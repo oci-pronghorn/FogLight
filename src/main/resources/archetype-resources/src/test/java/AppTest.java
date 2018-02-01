43f932afd908d959aa8c556ec69f1a164d0c06ed
Nathan Tippy <nathan@tippy.name>
2018-02-01 13:11:26 -0600

    change to new testing style

--- src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -18,22 +18,10 @@ public class AppTest {
 	 @Test
 	    public void testApp()
 	    {
-		    FogRuntime runtime = FogRuntime.test(new ${artifactId}());	    	
-	    	NonThreadScheduler scheduler = (NonThreadScheduler)runtime.getScheduler();    	
-	    	TestHardware hardware = (TestHardware)runtime.getHardware();
-	    
-	    	scheduler.startup();
-	    	
-	    	int iterations = 10;
-			while (--iterations >= 0) {
-				    		
-					scheduler.run();
-					
-					//test application here
-					
-			}
-			
-			scheduler.shutdown();
+		    int timeoutMS = 2000;
+		    GreenRuntime.testUntilShutdownRequested(new ${artifactId}(), timeoutMS);
+		    	    
 			
 	    }
+
 }

dd30aadf6a9c9c6cdddf845b3311953b9c6fbf69
Nathan Tippy <nathantippy@gmail.com>
2018-06-12 10:34:32 -0500

    added data splitter when loading data

--- src/main/java/com/ociweb/pronghorn/image/ImageGraphBuilder.java
+++ src/main/java/com/ociweb/pronghorn/image/ImageGraphBuilder.java
@@ -3,7 +3,9 @@ package com.ociweb.pronghorn.image;
 import com.ociweb.pronghorn.pipe.Pipe;
 import com.ociweb.pronghorn.pipe.RawDataSchema;
 import com.ociweb.pronghorn.stage.file.FileBlobReadStage;
+import com.ociweb.pronghorn.stage.route.ReplicatorStage;
 import com.ociweb.pronghorn.stage.scheduling.GraphManager;
+import com.ociweb.pronghorn.stage.test.PipeCleanerStage;
 
 public class ImageGraphBuilder {
 
@@ -19,11 +21,41 @@ public class ImageGraphBuilder {
 		Pipe<RawDataSchema> mDataB = RawDataSchema.instance.newPipe(8, 1<<12);
 		Pipe<RawDataSchema> mDataM = RawDataSchema.instance.newPipe(8, 1<<12);
 		
+		Pipe<RawDataSchema> mDataRRead = new Pipe(mDataR.config().grow2x());
+		Pipe<RawDataSchema> mDataGRead = new Pipe(mDataG.config().grow2x());
+		Pipe<RawDataSchema> mDataBRead = new Pipe(mDataB.config().grow2x());
+		Pipe<RawDataSchema> mDataMRead = new Pipe(mDataM.config().grow2x());
 		
-		FileBlobReadStage mappingDataFileReader = new FileBlobReadStage(gm, mData, dataFilePath);
+		Pipe<RawDataSchema> mDataRWrite = new Pipe(mDataR.config().grow2x());
+		Pipe<RawDataSchema> mDataGWrite = new Pipe(mDataG.config().grow2x());
+		Pipe<RawDataSchema> mDataBWrite = new Pipe(mDataB.config().grow2x());
+		Pipe<RawDataSchema> mDataMWrite = new Pipe(mDataM.config().grow2x());
 		
-		new RawDataSplitter(gm, mData, mDataR, mDataG, mDataB, mDataM);
 		
+		FileBlobReadStage readStage = FileBlobReadStage.newInstance(gm, mData, dataFilePath);		
+		RawDataSplitter.newInstance(gm, gm.getOutputPipe(gm, readStage),
+				                    mDataR, mDataG, mDataB, mDataM);
+		
+		ReplicatorStage.newInstance(gm, mDataR, mDataRRead, mDataRWrite);
+		ReplicatorStage.newInstance(gm, mDataG, mDataGRead, mDataGWrite);
+		ReplicatorStage.newInstance(gm, mDataB, mDataBRead, mDataBWrite);
+		ReplicatorStage.newInstance(gm, mDataM, mDataMRead, mDataMWrite);
+		
+		
+		
+		
+		//these added just as place holders for now.
+		
+		PipeCleanerStage.newInstance(gm, mDataRRead); //these go to the image mappping to location logic
+		PipeCleanerStage.newInstance(gm, mDataGRead);
+		PipeCleanerStage.newInstance(gm, mDataBRead);
+		PipeCleanerStage.newInstance(gm, mDataMRead);
+		
+		
+		PipeCleanerStage.newInstance(gm, mDataRWrite); //these go to the image learning mapping to location
+		PipeCleanerStage.newInstance(gm, mDataGWrite);
+		PipeCleanerStage.newInstance(gm, mDataBWrite);
+		PipeCleanerStage.newInstance(gm, mDataMWrite);
 		
 		
 		
--- src/main/java/com/ociweb/pronghorn/image/RawDataSplitter.java
+++ src/main/java/com/ociweb/pronghorn/image/RawDataSplitter.java
@@ -1,5 +1,7 @@
 package com.ociweb.pronghorn.image;
 
+import com.ociweb.pronghorn.pipe.DataInputBlobReader;
+import com.ociweb.pronghorn.pipe.DataOutputBlobWriter;
 import com.ociweb.pronghorn.pipe.Pipe;
 import com.ociweb.pronghorn.pipe.RawDataSchema;
 import com.ociweb.pronghorn.stage.PronghornStage;
@@ -9,7 +11,16 @@ public class RawDataSplitter extends PronghornStage {
 
 	private final Pipe<RawDataSchema> source; 
     private final Pipe<RawDataSchema>[] targets;
-    
+    private int targetPipeIdx = -1;
+    private long targetRemaining;
+	private DataInputBlobReader<RawDataSchema> inputStream;
+    private boolean isShuttingDown = false;
+	
+    public static RawDataSplitter newInstance(GraphManager gm, 
+            	Pipe<RawDataSchema> source, 
+            	Pipe<RawDataSchema> ... targets) {
+    	return new RawDataSplitter(gm, source, targets);
+    }
     
     /**
      * Read raw data and route blocks to the right target based on data found.
@@ -26,18 +37,80 @@ public class RawDataSplitter extends PronghornStage {
 		super(gm, source,  targets);
 		this.source = source;
 		this.targets = targets;
+		this.inputStream = Pipe.inputStream(source);
 	}
 
 	@Override
 	public void run() {
 		
-		while (Pipe.hasContentToRead(source)) {
+		processData();//even called when shutting down so we can clear the pipes.
+		
+		if (!isShuttingDown) {
+							
+			while (Pipe.hasContentToRead(source)) {
+				int msgIdx = Pipe.takeMsgIdx(source);
+				if (msgIdx>=0) {
+					DataInputBlobReader.accumLowLevelAPIField(inputStream);
+					Pipe.confirmLowLevelRead(source, Pipe.sizeOf(source, RawDataSchema.MSG_CHUNKEDSTREAM_1));
+					Pipe.readNextWithoutReleasingReadLock(source);
+									
+					processData();
+				} else {
+					isShuttingDown = true;
+				}
+			}
 			
+		} else {
+			//shutdown process
+			int i = targets.length;
+			while (--i>=0) {
+				if (!Pipe.hasRoomForWrite(targets[i])) {
+					return;//try again later
+				}
+			}
+			Pipe.publishEOF(targets);
+			requestShutdown();
 		}
-		
-		// TODO Auto-generated method stub
-		
 	}
+
+	private void processData() {
+		DataInputBlobReader<RawDataSchema> inputStream = Pipe.inputStream(source);
+		
+		if (targetPipeIdx<0) {
+			//need to read next block
+			if (inputStream.available( )>= 10) { //short plus long is 2 + 8
+				targetPipeIdx = inputStream.readShort();
+				targetRemaining = inputStream.readLong();
+				Pipe.releasePendingAsReadLock(source, 8);
+			}
+		}
 	
+		//move the data
+		while (targetRemaining>0 && inputStream.available()>0 && Pipe.hasRoomForWrite(targets[targetPipeIdx]) ) {
+			
+			Pipe<RawDataSchema> t = targets[targetPipeIdx];
+			int toCopyLength = (int)Math.min(Math.min(t.maxVarLen, targetRemaining), inputStream.available());
+			
+			int size = Pipe.addMsgIdx(t, RawDataSchema.MSG_CHUNKEDSTREAM_1);
+			DataOutputBlobWriter<RawDataSchema> outputStream = Pipe.openOutputStream(t);
+			inputStream.readInto(outputStream, toCopyLength);
+			DataOutputBlobWriter.closeLowLevelField(outputStream);
+			Pipe.confirmLowLevelWrite(t, size);
+			Pipe.publishWrites(t);
+			
+			Pipe.releasePendingAsReadLock(source, toCopyLength);
+							
+			targetRemaining -= toCopyLength;
+			if (targetRemaining<=0) {
+				if (inputStream.available( )>= 10) { //short plus long is 2 + 8
+					targetPipeIdx = inputStream.readShort();
+					targetRemaining = inputStream.readLong();
+					Pipe.releasePendingAsReadLock(source, 8);
+				} else {
+					targetPipeIdx = -1;
+				}
+			}
+		}
+	}
 
 }

6078b30d3f58bf238b04aae6027892350940a476
Brandon Sanders <brandon@alicorn.io>
2018-06-14 14:03:38 -0400

    Continue work on image downscale stage; add downscaling prototype.

--- src/main/java/com/ociweb/pronghorn/image/ImageDownscaleStage.java
+++ src/main/java/com/ociweb/pronghorn/image/ImageDownscaleStage.java
@@ -11,15 +11,14 @@ import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 /**
- * Image processing stage which accepts an ImageSchema pipe and writes to
- * multiple output pipes of the same type.
+ * Image processing stage which accepts an input image and outputs four
+ * image pipes at a downscaled resolution:
  *
- * - This stage splits the image into R, G, B, and Monochrome images.
- *
- * - This stage downscales the resolution of the image based on a configurable
- *   setting in the constructor. E.G., given a resolution of 100 x 100, this
- *   stage will produce four output pipes with a 100x100 image in r, g, b, and
- *   monochrome.
+ * - A R color channel pipe containing {@code outputWidth} x {@code outputHeight} bytes.
+ * - A G color channel pipe containing {@code outputWidth} x {@code outputHeight} bytes.
+ * - A B color channel pipe containing {@code outputWidth} x {@code outputHeight} bytes.
+ * - A Monochrome color channel pipe containing {@code outputWidth} x {@code outputHeight} bytes
+ *   where each byte is the average of the R, G, and B bytes at that pixel.
  *
  * @author Brandon Sanders [brandon@alicorn.io]
  */
@@ -32,23 +31,23 @@ public class ImageDownscaleStage extends PronghornStage {
     private final int outputWidth;
     private final ByteBuffer encodingBytes = ByteBuffer.wrap(new byte[32]);
 
-    // Working output.
+    // Input pipe information.
     private int imageFrameWidth = -1;
     private int imageFrameheight = -1;
     private int imageFrameSizeBytes = -1;
+    private int imageFrameRowsReceived = 0;
     private byte[] imageFrameRowBytes = null;
-    private byte[] imageFrameRowBytesR = null;
-    private byte[] imageFrameRowBytesG = null;
-    private byte[] imageFrameRowBytesB = null;
-    private byte[] imageFrameRowBytesMono = null;
+    private int[] imageFrameRowBytesDownsampled = null;
+    private final byte[] imageFrameRowBytesR;
+    private final byte[] imageFrameRowBytesG;
+    private final byte[] imageFrameRowBytesB;
+    private final byte[] imageFrameRowBytesMono;
 
-    // Pipe indexes.
+    // Pipe indices and encodings.
     public static final int R_OUTPUT_IDX = 0;
     public static final int G_OUTPUT_IDX = 1;
     public static final int B_OUTPUT_IDX = 2;
     public static final int MONO_OUTPUT_IDX = 3;
-
-    // Image encodings.
     public static final byte[] INPUT_ENCODING = "RGB24".getBytes(StandardCharsets.US_ASCII);
     public static final byte[] R_OUTPUT_ENCODING = "R8".getBytes(StandardCharsets.US_ASCII);
     public static final byte[] G_OUTPUT_ENCODING = "G8".getBytes(StandardCharsets.US_ASCII);
@@ -66,6 +65,12 @@ public class ImageDownscaleStage extends PronghornStage {
         // Assign configurations.
         this.outputHeight = outputHeight;
         this.outputWidth = outputWidth;
+
+        // Setup frame row buffers.
+        this.imageFrameRowBytesR = new byte[outputWidth];
+        this.imageFrameRowBytesG = new byte[outputWidth];
+        this.imageFrameRowBytesB = new byte[outputWidth];
+        this.imageFrameRowBytesMono = new byte[outputWidth];
     }
 
     @Override
@@ -127,79 +132,96 @@ public class ImageDownscaleStage extends PronghornStage {
 
                 case ImageSchema.MSG_FRAMECHUNK_2:
 
-                    // Calculate row length.
+                    // Calculate working frame sizes.
+                    int inputFrameColumnsPerOutputColumn = imageFrameWidth / outputWidth;
+                    int inputFrameRowsPerOutputFrameRow = imageFrameheight / outputHeight;
+
+                    // Determine row length.
                     int rowLength = PipeReader.readBytesLength(input, ImageSchema.MSG_FRAMECHUNK_2_FIELD_ROWBYTES_102);
 
                     // Prepare arrays if not already ready.
                     if (imageFrameRowBytes == null || imageFrameRowBytes.length != rowLength) {
                         imageFrameRowBytes = new byte[rowLength];
-                        imageFrameRowBytesR = new byte[imageFrameRowBytes.length / 3];
-                        imageFrameRowBytesG = new byte[imageFrameRowBytes.length / 3];
-                        imageFrameRowBytesB = new byte[imageFrameRowBytes.length / 3];
-                        imageFrameRowBytesMono = new byte[imageFrameRowBytes.length / 3];
+                        imageFrameRowBytesDownsampled = new int[outputWidth * 3];
                     }
 
                     // Read bytes into array.
                     PipeReader.readBytes(input, ImageSchema.MSG_FRAMECHUNK_2_FIELD_ROWBYTES_102, imageFrameRowBytes, 0);
 
-                    // Process channel data for bytes.
-                    int monoAvg = 0;
-                    int rPos = 0;
-                    int gPos = 0;
-                    int bPos = 0;
-                    int monoPos = 0;
-                    for (int i = 0; i < imageFrameRowBytes.length; i++) {
+                    // Downsample frame width.
+                    int i = 0;
+                    for (int j = 0; j < imageFrameRowBytes.length; j += 3) {
 
-                        // R
-                        if (i % 3 == 0) {
+                        // Add bytes to sum.
+                        imageFrameRowBytesDownsampled[i] += imageFrameRowBytes[j];
+                        imageFrameRowBytesDownsampled[i + 1] += imageFrameRowBytes[j + 1];
+                        imageFrameRowBytesDownsampled[i + 2] += imageFrameRowBytes[j + 2];
 
-                            // If this is not the first iteration, also compute the mono.
-                            if (i != 0) {
-                                monoAvg = monoAvg / 3;
-                                imageFrameRowBytesMono[monoPos] = (byte) monoAvg;
-                                monoAvg = 0;
-                                monoPos++;
-                            }
-
-                            imageFrameRowBytesR[rPos] = imageFrameRowBytes[i];
-                            monoAvg += imageFrameRowBytes[i];
-                            rPos++;
-
-                        // G
-                        } else if (i % 3 == 1) {
-                            imageFrameRowBytesG[gPos] = imageFrameRowBytes[i];
-                            monoAvg += imageFrameRowBytes[i];
-                            gPos++;
-
-                        // B
-                        } else if (i % 3 == 2) {
-                            imageFrameRowBytesB[bPos] = imageFrameRowBytes[i];
-                            monoAvg += imageFrameRowBytes[i];
-                            bPos++;
+                        // If we've summed enough bytes, move to next output pixel.
+                        if ((j / 3) >= inputFrameColumnsPerOutputColumn) {
+                            i += 3;
                         }
                     }
+                    assert i == imageFrameRowBytesDownsampled.length;
 
-                    assert rPos == imageFrameRowBytesR.length;
-                    assert gPos == imageFrameRowBytesG.length;
-                    assert bPos == imageFrameRowBytesB.length;
-                    assert monoPos == imageFrameRowBytesMono.length;
+                    // If we've summed enough frames to downsample height, generate an output frame.
+                    imageFrameRowsReceived++;
+                    if (imageFrameRowsReceived >= inputFrameRowsPerOutputFrameRow) {
 
-                    // Send channels to clients.
-                    // TODO: Also, downsample.
-                    for (int i = 0; i < outputs.length; i++) {
-                        if (PipeWriter.tryWriteFragment(outputs[i], ImageSchema.MSG_FRAMECHUNK_2)) {
-                            switch (i) {
-                                case R_OUTPUT_IDX:
-                                    PipeWriter.writeBytes(outputs[i], ImageSchema.MSG_FRAMECHUNK_2_FIELD_ROWBYTES_102, imageFrameRowBytesR, 0, rPos);
-                                case G_OUTPUT_IDX:
-                                    PipeWriter.writeBytes(outputs[i], ImageSchema.MSG_FRAMECHUNK_2_FIELD_ROWBYTES_102, imageFrameRowBytesG, 0, gPos);
-                                case B_OUTPUT_IDX:
-                                    PipeWriter.writeBytes(outputs[i], ImageSchema.MSG_FRAMECHUNK_2_FIELD_ROWBYTES_102, imageFrameRowBytesB, 0, bPos);
-                                case MONO_OUTPUT_IDX:
-                                    PipeWriter.writeBytes(outputs[i], ImageSchema.MSG_FRAMECHUNK_2_FIELD_ROWBYTES_102, imageFrameRowBytesMono, 0, monoPos);
+                        // Divide image frames by total pixels per cell.
+                        int inputPixelsPerOutputPixel = inputFrameColumnsPerOutputColumn * inputFrameRowsPerOutputFrameRow;
+                        for (i = 0; i < imageFrameRowBytesDownsampled.length; i += 3) {
+                            imageFrameRowBytesDownsampled[i] = imageFrameRowBytesDownsampled[i] / inputPixelsPerOutputPixel;
+                            imageFrameRowBytesDownsampled[i + 1] = imageFrameRowBytesDownsampled[i + 1] / inputPixelsPerOutputPixel;
+                            imageFrameRowBytesDownsampled[i + 2] = imageFrameRowBytesDownsampled[i + 2] / inputPixelsPerOutputPixel;
+                        }
+
+                        // Extract RGB and Mono channels.
+                        i = 0;
+                        for (int j = 0; j < imageFrameRowBytesDownsampled.length; j += 3) {
+
+                            // Extract RGB channels.
+                            imageFrameRowBytesR[i] = (byte) imageFrameRowBytesDownsampled[j];
+                            imageFrameRowBytesG[i] = (byte) imageFrameRowBytesDownsampled[j + 1];
+                            imageFrameRowBytesB[i] = (byte) imageFrameRowBytesDownsampled[j + 2];
+
+                            // Average bytes into mono channel.
+                            int temp = 0;
+                            temp += imageFrameRowBytesR[i];
+                            temp += imageFrameRowBytesG[i];
+                            temp += imageFrameRowBytesB[i];
+                            temp = temp / 3;
+                            imageFrameRowBytesMono[i] = (byte) temp;
+
+                            // Progress counter.
+                            i++;
+                        }
+
+                        // Send channels to clients.
+                        // TODO: Refactor so that if a try-write fails, the row will be written during the next time slice.
+                        for (i = 0; i < outputs.length; i++) {
+                            if (PipeWriter.tryWriteFragment(outputs[i], ImageSchema.MSG_FRAMECHUNK_2)) {
+                                switch (i) {
+                                    case R_OUTPUT_IDX:
+                                        PipeWriter.writeBytes(outputs[i],
+                                                              ImageSchema.MSG_FRAMECHUNK_2_FIELD_ROWBYTES_102,
+                                                              imageFrameRowBytesR, 0, imageFrameRowBytesR.length);
+                                    case G_OUTPUT_IDX:
+                                        PipeWriter.writeBytes(outputs[i],
+                                                              ImageSchema.MSG_FRAMECHUNK_2_FIELD_ROWBYTES_102,
+                                                              imageFrameRowBytesG, 0, imageFrameRowBytesG.length);
+                                    case B_OUTPUT_IDX:
+                                        PipeWriter.writeBytes(outputs[i],
+                                                              ImageSchema.MSG_FRAMECHUNK_2_FIELD_ROWBYTES_102,
+                                                              imageFrameRowBytesB, 0, imageFrameRowBytesB.length);
+                                    case MONO_OUTPUT_IDX:
+                                        PipeWriter.writeBytes(outputs[i],
+                                                              ImageSchema.MSG_FRAMECHUNK_2_FIELD_ROWBYTES_102,
+                                                              imageFrameRowBytesMono, 0, imageFrameRowBytesMono.length);
+                                }
+
+                                PipeWriter.publishWrites(outputs[i]);
                             }
-
-                            PipeWriter.publishWrites(outputs[i]);
                         }
                     }
 

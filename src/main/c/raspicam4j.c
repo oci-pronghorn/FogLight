58325572693177f5badb90957ea80a73514cfb58
Brandon Sanders <brandon@alicorn.io>
2018-06-19 19:04:57 -0400

    Revert to simpler buffer design for now.

--- src/main/c/raspicam4j.c
+++ src/main/c/raspicam4j.c
@@ -1,4 +1,3 @@
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,23 +25,12 @@
 #define true (1==1)
 #define false (!true)
 
-// Structure for storing a buffer's meta-data.
-struct Buffer {
-    void* start;
-    size_t size;
-    struct v4l2_buffer info;
-    int fd;
-};
-
-// Array of our buffers.
-#define BUFFERS_COUNT 2
-struct Buffer buffers[BUFFERS_COUNT];
-int nextBufferToDequeue = 0;
-
-// Userspace buffer object.
-int userBufferCreated = false;
-jfieldID byteBufferAddressField;
-jobject userByteBuffer;
+// Shared buffer info across all calls.
+// TODO: If open is called multiple times, the behavior of this variable will be undefined.
+struct v4l2_buffer bufferinfo;
+void* buffer_start;
+int buffer_size = -1;
+int buffering = false;
 
 JNIEXPORT jint JNICALL Java_com_ociweb_iot_camera_RaspiCam_open(JNIEnv *env, jobject object, jstring device, jint width, jint height) {
     const char *actualDevice = (*env)->GetStringUTFChars(env, device, NULL);
@@ -84,11 +72,14 @@ JNIEXPORT jint JNICALL Java_com_ociweb_iot_camera_RaspiCam_open(JNIEnv *env, job
         	fprintf(stderr, "Camera used %d bytes per line instead of the expected %d.\n", format.fmt.pix.bytesperline, width * 3);
         }
 
-        // Request buffers.
+        // Get buffer size based on W x H.
+        buffer_size = format.fmt.pix.sizeimage;
+
+        // Setup buffer format.
         struct v4l2_requestbuffers bufrequest;
         bufrequest.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         bufrequest.memory = V4L2_MEMORY_MMAP;
-        bufrequest.count = BUFFERS_COUNT;
+        bufrequest.count = 1;
 
         // Configure buffer format.
         if (v4l2_ioctl(fd, VIDIOC_REQBUFS, &bufrequest) < 0) {
@@ -98,60 +89,49 @@ JNIEXPORT jint JNICALL Java_com_ociweb_iot_camera_RaspiCam_open(JNIEnv *env, job
         }
 
         // Allocate buffers.
-        for (int i = 0; i < BUFFERS_COUNT; i++) {
-            struct Buffer buffer;
+        memset(&bufferinfo, 0, sizeof(bufferinfo));
 
-            // Set buffer file descriptor.
-            buffer.fd = fd;
+        bufferinfo.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        bufferinfo.memory = V4L2_MEMORY_MMAP;
+        bufferinfo.index = 0;
 
-            // Get buffer size.
-            buffer.size = format.fmt.pix.sizeimage;
-
-            // Allocate buffer information.
-            memset(&buffer.info, 0, sizeof(buffer.info));
-            buffer.info.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-            buffer.info.memory = V4L2_MEMORY_MMAP;
-            buffer.info.index = i;
-
-            // Configure buffer with V4L2.
-            if (v4l2_ioctl(fd, VIDIOC_QUERYBUF, &buffer.info) < 0) {
-                v4l2_close(fd);
-                fprintf(stderr, "Could not configure query buffers.\n");
-                return -1; // TODO: More descriptive error?
-            }
-
-            // Memory map buffer data.
-            buffer.start = v4l2_mmap(
-                NULL,
-                buffer.info.length,
-                PROT_READ | PROT_WRITE,
-                MAP_SHARED,
-                fd,
-                buffer.info.m.offset
-            );
-
-            // Validate memory map succeeded.
-            if (buffer.start == MAP_FAILED) {
-                v4l2_close(fd);
-                fprintf(stderr, "Could not memory map camera.\n");
-                return -1; // TODO: More descriptive error?
-            }
-
-            // Clean buffer data.
-            memset(buffer.start, 0, buffer.info.length);
-
-            // Place buffer in array.
-            buffers[i] = buffer;
-        }
-
-        // Put the first buffer in the incoming queue.
-        if (v4l2_ioctl(fd, VIDIOC_QBUF, &(buffers[0].info)) < 0) {
-            fprintf(stderr, "Could not queue buffer (during open) with error: %d.\n", errno);
+        // Configure buffers.
+        if (v4l2_ioctl(fd, VIDIOC_QUERYBUF, &bufferinfo) < 0) {
+            v4l2_close(fd);
+            fprintf(stderr, "Could not configure query buffers.\n");
             return -1; // TODO: More descriptive error?
         }
 
+        // Memory map buffers.
+        buffer_start = v4l2_mmap(
+            NULL,
+            bufferinfo.length,
+            PROT_READ | PROT_WRITE,
+            MAP_SHARED,
+            fd,
+            bufferinfo.m.offset
+        );
+
+        // Validate memory map succeeded.
+        if (buffer_start == MAP_FAILED) {
+            v4l2_close(fd);
+            fprintf(stderr, "Could not memory map camera.\n");
+            return -1; // TODO: More descriptive error?
+        }
+
+        // Clean struct.
+        memset(buffer_start, 0, bufferinfo.length);
+
+        // Put the buffer in the incoming queue.
+        if (v4l2_ioctl(fd, VIDIOC_QBUF, &bufferinfo) < 0) {
+            fprintf(stderr, "Could not queue buffer (during open).\n");
+            return -1; // TODO: More descriptive error?
+        } else {
+            buffering = true;
+        }
+
         // Activate streaming
-        if (v4l2_ioctl(fd, VIDIOC_STREAMON, &(buffers[0].info.type)) < 0) {
+        if (v4l2_ioctl(fd, VIDIOC_STREAMON, &bufferinfo.type) < 0) {
             v4l2_close(fd);
             fprintf(stderr, "Could not activate streaming.\n");
             return -1; // TODO: More descriptive error?
@@ -163,56 +143,51 @@ JNIEXPORT jint JNICALL Java_com_ociweb_iot_camera_RaspiCam_open(JNIEnv *env, job
 }
 
 JNIEXPORT jobject JNICALL Java_com_ociweb_iot_camera_RaspiCam_getFrameBuffer(JNIEnv *env, jobject object, jint fd) {
-    if (!userBufferCreated) {
-        jclass byteBufferClass = (*env)->FindClass(env, "java/nio/Buffer");
-        byteBufferAddressField = (*env)->GetFieldID(env, byteBufferClass, "address", "J");
-        userByteBuffer = (*env)->NewDirectByteBuffer(env, buffers[0].start, buffers[0].size);
-        userBufferCreated = true;
-    }
-
-    return userByteBuffer;
+    return (*env)->NewDirectByteBuffer(env, buffer_start, buffer_size);
 }
 
 JNIEXPORT jint JNICALL Java_com_ociweb_iot_camera_RaspiCam_readFrame(JNIEnv *env, jobject object, jint fd) {
 
-    // Fail if the user hasn't initialized their buffer yet.
-    if (!userBufferCreated) {
-        fprintf(stderr, "User must invoke getFrameBuffer at least once before reading a frame.");
-        return -1;
+    // Put the buffer in the incoming queue if we are not currently buffering.
+    // We do not automatically re-insert the buffer into the camera's queue
+    // in order to prevent the user's data from being overwritten before they
+    // are ready to read the next frame.
+    if (!buffering) {
+        if (v4l2_ioctl(fd, VIDIOC_QBUF, &bufferinfo) < 0) {
+            fprintf(stderr, "Could not queue buffer (during read).\n");
+        } else {
+            buffering = true;
+        }
     }
 
-    // The buffer's waiting in the outgoing queue.
+    // Read data from the buffer in the incoming queue.
     // If -1 is returned, the buffer isn't ready to read yet.
-    memset(&(buffers[nextBufferToDequeue].info), 0, sizeof(buffers[nextBufferToDequeue].info));
-    buffers[nextBufferToDequeue].info.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    buffers[nextBufferToDequeue].info.memory = V4L2_MEMORY_USERPTR;
-    if (v4l2_ioctl(fd, VIDIOC_DQBUF, &(buffers[nextBufferToDequeue].info)) < 0) {
-        if (errno != EAGAIN) {
-            fprintf(stderr, "Unknown error code %d when reading frame from camera.", errno);
+    // The buffer most likely will not be ready to read immediately.
+    if (buffering) {
+        memset(&(bufferinfo), 0, sizeof(bufferinfo));
+        bufferinfo.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        bufferinfo.memory = V4L2_MEMORY_USERPTR;
+        if (v4l2_ioctl(fd, VIDIOC_DQBUF, &bufferinfo) < 0) {
+            if (errno != EAGAIN) {
+                fprintf(stderr, "Unknown error code %d when reading frame from camera.");
+            }
+
+            return -1;
+        } else {
+            buffering = false;
         }
 
-        return -1;
+        return bufferinfo.length;
     }
 
-    // Set user buffer address to the newly filled buffer.
-    (*env)->SetLongField(env, userByteBuffer, byteBufferAddressField, (intptr_t) buffers[nextBufferToDequeue].start);
-
-    // Increment next buffer.
-    nextBufferToDequeue++;
-    nextBufferToDequeue = nextBufferToDequeue % BUFFERS_COUNT;
-
-    // Put the buffer in the incoming queue.
-    if (v4l2_ioctl(fd, VIDIOC_QBUF, &(buffers[nextBufferToDequeue].info)) < 0) {
-        fprintf(stderr, "Could not queue buffer (during read).\n");
-    }
-
-    return buffers[0].info.length;
+    // If we either can't queue the buffer or we simply aren't buffering, return.
+    return -1;
 }
 
 JNIEXPORT jint JNICALL Java_com_ociweb_iot_camera_RaspiCam_close(JNIEnv *env, jobject object, jint fd) {
 
     // Deactivate streaming
-    if (v4l2_ioctl(fd, VIDIOC_STREAMOFF, &(buffers[0].info.type)) < 0){
+    if (v4l2_ioctl(fd, VIDIOC_STREAMOFF, &bufferinfo.type) < 0){
         return -1; // TODO: More descriptive error?
     } else {
         v4l2_close(fd);

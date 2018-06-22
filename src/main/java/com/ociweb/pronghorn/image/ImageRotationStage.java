491210d9765e11f35314af0175f1c277f21f7ffe
Brandon Sanders <brandon@alicorn.io>
2018-06-22 11:52:50 -0400

    Cleanup image capture stage a bit.

--- src/main/java/com/ociweb/pronghorn/image/ImageRotationStage.java
+++ src/main/java/com/ociweb/pronghorn/image/ImageRotationStage.java
@@ -1,5 +1,6 @@
 package com.ociweb.pronghorn.image;
 
+import com.ociweb.iot.maker.LinuxImageCaptureStage;
 import com.ociweb.pronghorn.image.schema.ImageRotationSchema;
 import com.ociweb.pronghorn.image.schema.ImageSchema;
 import com.ociweb.pronghorn.pipe.Pipe;
@@ -9,12 +10,26 @@ import com.ociweb.pronghorn.stage.scheduling.GraphManager;
 /**
  * Stage for rotating and cropping images.
  *
+ * This stage consumes and produces frames that are in the RGB24 encoding.
+ *
  * @author Brandon Sanders [brandon@alicorn.io]
  */
 public class ImageRotationStage extends PronghornStage {
 
-    public static final int DEFAULT_CROP_WIDTH = 1280;
-    public static final int DEFAULT_CROP_HEIGHT = 720;
+    private final Pipe<ImageSchema> imagePipeIn;
+    private final Pipe<ImageRotationSchema> imageRotationPipeIn;
+    private final Pipe<ImageSchema> imagePipeOut;
+
+    private final int cropWidth;
+    private final int cropHeight;
+
+    private final byte[] outputFrame;
+    private int outputFrameHead = 0;
+    private byte[] inputFrame;
+    private int inputFrameHead = 0;
+
+    public static final int DEFAULT_CROP_WIDTH = LinuxImageCaptureStage.DEFAULT_FRAME_WIDTH;
+    public static final int DEFAULT_CROP_HEIGHT = LinuxImageCaptureStage.DEFAULT_FRAME_HEIGHT;
 
     public ImageRotationStage(GraphManager gm,
                               Pipe<ImageSchema> imagePipeIn,
@@ -29,15 +44,27 @@ public class ImageRotationStage extends PronghornStage {
                               Pipe<ImageSchema> imagePipeOut,
                               int cropWidth, int cropHeight) {
         super(gm, new Pipe[] {imagePipeIn, imageRotationPipeIn}, imagePipeOut);
-    }
 
-    @Override
-    public void startup() {
+        // Setup constant fields.
+        this.imagePipeIn = imagePipeIn;
+        this.imageRotationPipeIn = imageRotationPipeIn;
+        this.imagePipeOut = imagePipeOut;
+        this.cropWidth = cropWidth;
+        this.cropHeight = cropHeight;
 
+        // Calculate output frame size based on RGB24 encoding.
+        this.outputFrame = new byte[this.cropWidth * this.cropHeight * 3];
     }
 
     @Override
     public void run() {
 
+    }
+
+    @Override
+    public void shutdown() {
+        if (Pipe.hasRoomForWrite(imagePipeOut, Pipe.EOF_SIZE)) {
+            Pipe.publishEOF(imagePipeOut);
+        }
     }
 }
